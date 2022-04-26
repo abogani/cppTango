56a3d3cc0c800363a85891c2a8ef44bc8a2b5e81
Thomas Juerges <203795-tjuerges@users.noreply.gitlab.com>
2022-04-26 09:11:26 +0100

    Make starting a Device Server on macOS from any directory on the system possible
    Correct the __darwin__ specific code in void DServer::class_factory() to
    make it possible to start a device server on macOS from any directory on the
    system. With the old code the DS could not be started unless the DS executable
    was run from the same DS executable's directory. Naturally this was very rarely
    the case and hence this really needed to be fixed in order to work for any
    directory.
    
    General modifications:
    - Introduce const std::string& Util::get_ds_unmodified_exec_name() which returns
      argv[0] of a Device Server executable. argv[0] is stored in the private
      member ds_unmodified_exec_name in Util.
    
    Modifications for macOS only:
    - Call get_ds_unmodified_exec_name from void DServer::class_factory() to
      to retrieve the device server's executable name and resolve the full path
      of it with the help of realpath from the std. C library.
    - Use the full path when dlopen the DS executable.
    - Significantly improve error messages. When dlsym cannot find the
      mangled class factory symbol in a Device Server file that was opened with
      dlopen, then the error message needs to tell the user what went wrong and if
      possible why.
    - Add documentation of why something is done.
    - Overwriting of the union member needs to happen after initialisation.

--- cppapi/server/class_factory.cpp
+++ cppapi/server/class_factory.cpp
@@ -39,9 +39,12 @@
 
 #include <tango/server/dserver.h>
 #ifdef __darwin__
+#include <dlfcn.h>
 #include <cstdlib>
 #include <cstring>
+#include <vector>
 #include <string>
+#include <libgen.h>
 #include <algorithm>
 #include <regex>
 #endif
@@ -76,18 +79,13 @@ typedef union _convertor
 	PTR d;
 	FARPROC s;
 }convertor;
-#endif
-
-
-#ifdef __darwin__
-#include <dlfcn.h>
-
+#elif defined(__darwin__)
 typedef void (DServer::*PTR)(void);
 typedef union _convertor
 {
 	PTR d;
 	void *s;
-}convertor;
+} convertor;
 #endif
 
 
@@ -117,7 +115,7 @@ void DServer::class_factory()
 
 #ifdef _WIN64
 	if ((proc = GetProcAddress(mod,"?class_factory@DServer@Tango@@AEAAXXZ")) == NULL)
-#elif _WIN32 /* WIN32 */
+#elif defined(_WIN32) /* WIN32 */
 	if ((proc = GetProcAddress(mod,"?class_factory@DServer@Tango@@AAEXXZ")) == NULL)
 #endif
 	{
@@ -133,43 +131,128 @@ void DServer::class_factory()
 		(this->*tmp)();
 	}
 
-#elif __darwin__
-	Tango::Util *tg = Tango::Util::instance();
-	std::string exe_name = tg->get_ds_exec_name();
-	exe_name = exe_name;
+#elif defined(__darwin__)
+    Tango::Util* tg{Tango::Util::instance()};
+    const std::string& exe_name{tg->get_ds_unmodified_exec_name()};
 
-	void *mod;
-	void *proc;
-	convertor conv;
-	PTR tmp;
+    // This will split a string at each ":" and return a
+    // std::vector< std::string > with each string between ":"s as an item.
+    auto split = [](const std::string& path_env,
+        const std::string delimiter = ":") -> std::vector< std::string >
+    {
+        std::regex regex{delimiter};
+        return {std::sregex_token_iterator(std::begin(path_env),
+            std::end(path_env), regex, -1), std::sregex_token_iterator()};
+    };
 
-	if ((mod = dlopen (exe_name.c_str(), RTLD_LAZY )) == NULL)
-	{
-		std::cerr << "Oops, no class defined in this server. Exiting ..." << std::endl;
-		exit(-1);
-	}
+    // Fetch the ${PATHS} environment variable.
+    const std::string path_environment{std::getenv("PATH")};
+    // Call the lambda function to split it at every ":".
+    auto paths{split(path_environment)};
+    // Now the std::vector paths should contain one path per entry.
 
-//
-// Use the mangled name to find the user DServer::class_factory method
-//
-// Due to the fact that on Windows 64 bits we have both WIN32 and WIN64
-// defined, start by testing WIN64 (See tango_config.h)
-//
+    // Figure out what the full path of exe_name = argv[0] is.
+    char full_path_exe_name[PATH_MAX]{};
+    // If the full path for argv[0] exists, then insert only its path
+    // at position 0 of the paths vector.
+    if(realpath(exe_name.c_str(), full_path_exe_name) != nullptr)
+    {
+        paths.insert(std::begin(paths), dirname(full_path_exe_name));
+    }
 
-	if ((proc = dlsym (mod,"_ZN5Tango7DServer13class_factoryEv")) == NULL)
-	{
-		std::cerr << "error : " << dlerror() << std::endl;
-		std::cerr << "Oops, no class defined in this server. Exiting ..." << std::endl;
-		exit(-1);
-	}
-	else
-	{
-		conv.d = &DServer::stop_polling;
-		conv.s = proc;
+    // The Tango::DServer::class_factory() symbol which an executable needs to
+    // define. Only then an executable contains a Tango Device.
+    const std::string darwin_class_factory_symbol
+        {"_ZN5Tango7DServer13class_factoryEv"};
 
-		tmp = conv.d;
-		(this->*tmp)();
-	}
+    // Shortened name of the executable, contains just the name
+    // of the executable without any "/" or ".".
+    const std::string shortened_exe_name{tg->get_ds_exec_name()};
+
+    // A pointer to the resolved Device class symbol.
+    void* proc{nullptr};
+
+    // A lambda funtion that checks if a file exists in a path.
+    // It checks all of the following to be true:
+    // - Can the file be accessed?
+    // - Is the file not a directory but a file?
+    // -
+    // I will iterate over every entry in paths and call this lambda.
+    auto file_exists_in_path = [&darwin_class_factory_symbol,
+        &shortened_exe_name, &proc]
+        (const std::string& path) -> bool
+    {
+        const std::string full_path{path + "/" + shortened_exe_name};
+        // Check if the executable exists in the path and that it is not
+        // a directory of the same name.
+        struct stat exec_file_stat{};
+        if((stat(full_path.c_str(), &exec_file_stat) == 0)
+            &&  ((exec_file_stat.st_mode & S_IFDIR) == 0))
+        {
+            // full_path should contain the full path to the Device
+            // Server's executable and hence a call to dlopen should be
+            // successful. This verifies if the file is actually an executable
+            // which contains symbols and is not just an executable shell
+            // script.
+            // So, try to dlopen the executable. This will fail for scripts.
+            void* mod{};
+            if((mod = dlopen(full_path.c_str(), RTLD_LAZY)) != nullptr)
+            {
+                // Now try to resolve darwin_class_factory_symbol.
+                if ((proc = dlsym(mod, darwin_class_factory_symbol.c_str()))
+                    != nullptr)
+                {
+                    return true;
+                }
+
+                std::cerr << "Warning: When inspecting the file \""
+                    << full_path
+                    << "\". The symbol for the Tango Class Factory ("
+                    << darwin_class_factory_symbol
+                    << ") could not be found. This likely means that the "
+                        "inspected file does not contain a Tango Class "
+                        "Factory . The OS error is:\n\t"
+                    << dlerror()
+                    << "\nThis could indicate that this file is a shell script "
+                        "of the same name.\n";
+            }
+        }
+        return false;
+    };
+
+    // Call file_exists_in_path for every item in paths until
+    // file_exists_in_path returns true. paths_iterator will then point
+    // to the correct value in paths. This then means that we have
+    // found the executable, dlopen was able to load it and it contains the
+    // symbol for the Tango class factory.
+    // If the executable has still not been found, then something is wrong
+    // and we exit. This also automatically covers the case when paths and
+    // full_path_exe_name are empty.
+    auto paths_iterator{std::find_if(std::begin(paths), std::end(paths),
+        file_exists_in_path)};
+
+    if(paths_iterator == std::end(paths))
+    {
+        std::cerr << "The Device Server's executable \""
+            << exe_name
+            << "\" cannot be located on this system.\n This means that the "
+                "Device Server cannot be started. This usually happens if the "
+                "executable for the Device Server is in one of the directories "
+                "in ${PATH}. Unfortunately could the executable not be found "
+                "in any of the PATH directories. This is what you can do now: "
+                "Start the Device Server again, but run it by providing the "
+                "full path name plus the executable, e.g. "
+                "/opt/foo/MyDeviceServer.\n";
+            // Exit with the proper error code.
+            exit(-ENOENT);
+    }
+
+    // proc contains a valid pointer to Tango::DServer::class_factory().
+    // Convert and call it.
+    convertor conv{.d = &DServer::stop_polling};
+    conv.s = proc;
+    PTR tmp{conv.d};
+    (this->*tmp)();
 #else
 		std::cerr << "Oops, no class defined in this server. Exiting ..." << std::endl;
 		exit(-1);
