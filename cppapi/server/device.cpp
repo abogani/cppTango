abogani/cppTango

commit 0acf491344bd7e02de53b2be0be5188b4e92183e

change input attribute as const reference

cppapi/server/device.cpp
@@ -3370,7 +3370,7 @@ void DeviceImpl::remove_attribute(Tango::Attr *rem_attr, bool free_it, bool clea
     std::vector<std::string> &poll_attr = get_polled_attr();
     std::vector<std::string>::iterator ite_attr;
 
-    std::string attr_name_low(attr_name);
+    std::string &attr_name_low(attr_name);
     std::transform(attr_name_low.begin(), attr_name_low.end(), attr_name_low.begin(), ::tolower);
 
 //
@@ -3977,7 +3977,7 @@ void DeviceImpl::poll_lists_2_v5()
 //
 //-------------------------------------------------------------------------------------------------------------------
 
-void DeviceImpl::init_cmd_poll_ext_trig(std::string cmd_name)
+void DeviceImpl::init_cmd_poll_ext_trig(const std::string &cmd_name)
 {
     std::string cmd_lowercase(cmd_name);
     std::transform(cmd_lowercase.begin(), cmd_lowercase.end(), cmd_lowercase.begin(), ::tolower);
@@ -4173,7 +4173,7 @@ void DeviceImpl::init_cmd_poll_period()
 //
 //------------------------------------------------------------------------------------------------------------------
 
-void DeviceImpl::init_attr_poll_ext_trig(std::string attr_name)
+void DeviceImpl::init_attr_poll_ext_trig(const std::string &attr_name)
 {
     std::string attr_lowercase(attr_name);
     std::transform(attr_lowercase.begin(), attr_lowercase.end(), attr_lowercase.begin(), ::tolower);

cppapi/server/classattribute.cpp
@@ -229,7 +229,7 @@ void MultiClassAttribute::init_class_attribute(const std::string &class_name,lon
 		{
 			std::vector<AttrProperty> prop_list;
 
-			std::string attr_name = db_list[ind].name;
+			const std::string &attr_name = db_list[ind].name;
 			long nb_prop = 0;
 			db_list[ind] >> nb_prop;
 

commit 3120207a3b9e8ceb3d6085f5db58967ecea1ba45

Fix regression with dynamic attributes (#1022)
Fix a regression introduced in commit 0acf491344bd7e02de53b2be0be5188b4e92183e.  This regression was preventing in some use cases the removal of some dynamic
attributes and exceptions like the following were thrown:

```
DevFailed[
DevError[
    desc = Device --> a/b/c
           Property writable_attr_name for attribute attr1 is set to Attr1, but this attribute does not exist
  origin = void Tango::MultiAttribute::check_associated(long int, const string&) at (/home/conda/feedstock_root/build_artifacts/cpptango_1667919173430/work/cppapi/server/multiattribute.cpp:570)
  reason = API_AttrOptProp
severity = ERR]

DevError[
    desc = Attribute Attr1 is not defined as attribute for your device.
           Can't remove it
  origin = void Tango::DeviceImpl::remove_attribute(const string&, bool, bool) at (/home/conda/feedstock_root/build_artifacts/cpptango_1667919173430/work/cppapi/server/device.cpp:3784)
  reason = API_AttrNotFound
severity = ERR]
]
```

cppapi/server/device.cpp
@@ -3610,7 +3610,7 @@ void DeviceImpl::remove_attribute(Tango::Attr *rem_attr, bool free_it, bool clea
     std::vector<std::string> &poll_attr = get_polled_attr();
     std::vector<std::string>::iterator ite_attr;
 
-    std::string &attr_name_low(attr_name);
+    std::string attr_name_low(attr_name);
     std::transform(attr_name_low.begin(), attr_name_low.end(), attr_name_low.begin(), ::tolower);
 
 //