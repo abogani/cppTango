fe2148f55e0ec45aac41d94c8aec84ff23481d3f
Thomas Braun <thomas.braun@byte-physics.de>
2023-01-31 14:32:55 +0100

    CMake: Add set_cflags_and_include
    This avoids duplicating the long list of default flags and include
    directories.
    
    In a future commit we want to use this list for more targets.

---
+++ configure/functions.cmake
@@ -0,0 +1,11 @@
+# Add the default cflags and include directories to ${target}
+function(set_cflags_and_include target)
+  target_compile_options(${target} PUBLIC ${ZMQ_PKG_CFLAGS_OTHER}
+                                          ${OMNIORB_PKG_CFLAGS_OTHER}
+                                          ${OMNICOS_PKG_CFLAGS_OTHER}
+                                          ${OMNIDYN_PKG_CFLAGS_OTHER})
+
+  target_include_directories(${target} SYSTEM PUBLIC ${ZMQ_PKG_INCLUDE_DIRS}
+                                                     ${OMNIORB_PKG_INCLUDE_DIRS}
+                                                     ${OMNIDYN_PKG_INCLUDE_DIRS})
+endfunction()
