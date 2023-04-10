abogani/cppTango

commit 17ec13bf6344bb641dfdfb3c6a92e714494e9c10

configure: Check omniorb version and omniidl code generation
Check that the omniorb libraries found by PkgConfig are version 4.2.2 or
later, which has fixed the bug in IDL union C++ code generation.

Moved locating the omniidl binary to
configure/find_and_check_omniidl.cmake. Check the omniidl C++ code
generation for the union bug and reject the use of buggy omniidl.

configure/find_and_check_omniidl.cmake
@@ -1,0 +1,65 @@
+if (NOT WIN32 AND NOT DEFINED OMNIORB_PKG_LIBRARIES)
+    return()
+endif()
+
+message("Using OMNIIDL_PATH=${OMNIIDL_PATH}")
+
+find_program(OMNIIDL NAMES omniidl
+                     HINTS ${OMNIIDL_PATH})
+
+message(STATUS "Testing omniidl for bug in generated c++ for IDL union")
+execute_process(
+    COMMAND ${OMNIIDL} -bcxx -Wbh=.h -Wbs=SK.cpp -Wbd=DynSK.cpp -Wba ${CMAKE_SOURCE_DIR}/configure/test_omniidl.idl
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    RESULT_VARIABLE OMNIIDL_RESULT
+    OUTPUT_VARIABLE OMNIIDL_OUTPUT
+    OUTPUT_VARIABLE OMNIIDL_ERROR
+    )
+
+if (NOT OMNIIDL_RESULT EQUAL 0)
+    message(WARNING "Failed to generate c++ code with omniidl:\n${OMNIIDL_OUTPUT}\n${OMNIIDL_ERROR}")
+    set(OMNIIDL FALSE)
+    return()
+endif()
+
+# Use a function to introduce a scope so that we can set CMAKE_CXX_FLAGS
+function(test_omniidl)
+    if (WIN32)
+        # Always statically link on Windows so we don't need the omniorb dll's to be in the PATH
+        set(CMAKE_CXX_FLAGS_DEBUG "/MTd")
+        set(link_libs ${OMNIORB_PKG_LIBRARIES_STA_DEBUG};ws2_32.lib)
+        set(defs "-D__x86__" "-D__NT__" "-D__OSVERSION__=4" "-D__WIN32__" "-D_WIN32_WINNT=0x0400")
+        foreach(def ${static_defs})
+            list(APPEND defs "-D${def}")
+        endforeach()
+    else()
+        set(link_libs ${OMNIORB_PKG_LIBRARIES} ${OMNICOS_PKG_LIBRARIES} ${OMNIDYN_PKG_LIBRARIES})
+        set(defs "")
+    endif()
+    set(inc_dirs "${CMAKE_CURRENT_BINARY_DIR};${OMNIORB_PKG_INCLUDE_DIRS};${OMNIDYN_PKG_INCLUDE_DIRS};${OMNICOS_PKG_INCLUDE_DIRS}")
+    set(link_dirs "${OMNIORB_PKG_LIBRARY_DIRS};${OMNIDYN_PKG_LIBRARY_DIRS};${OMNICOS_PKG_LIBRARY_DIRS}")
+
+    try_run(OMNIIDL_TEST_RUN OMNIIDL_TEST_COMPILE
+        ${CMAKE_CURRENT_BINARY_DIR}/
+        SOURCES ${CMAKE_SOURCE_DIR}/configure/test_omniidl.cpp ${CMAKE_CURRENT_BINARY_DIR}/test_omniidlSK.cpp ${CMAKE_CURRENT_BINARY_DIR}/test_omniidlDynSK.cpp
+        CMAKE_FLAGS
+            "-DINCLUDE_DIRECTORIES=${inc_dirs}"
+            "-DLINK_DIRECTORIES=${link_dirs}"
+        COMPILE_DEFINITIONS ${defs}
+        LINK_LIBRARIES ${link_libs}
+        COMPILE_OUTPUT_VARIABLE OMNIIDL_TEST_COMPILE_OUTPUT
+        )
+
+    if (NOT OMNIIDL_TEST_COMPILE)
+        message(WARNING "Failed to compile omniidl test program:\n${OMNIIDL_TEST_COMPILE_OUTPUT}")
+        set(OMNIIDL FALSE)
+        return()
+    endif()
+
+    if (NOT OMNIIDL_TEST_RUN EQUAL 0)
+        message(WARNING "${OMNIIDL} has bug in c++ code generation, will not use.")
+        set(OMNIIDL FALSE)
+    endif()
+endfunction()
+
+test_omniidl()

configure/CMakeLists.txt
@@ -47,29 +47,35 @@ if(TANGO_OMNI_BASE)
     set(OMNIORB_PKG_INCLUDE_DIRS ${TANGO_OMNI_BASE}/include)
     if(WIN32)
         set(OMNIIDL_PATH ${TANGO_OMNI_BASE}/bin/x86_win32/)
-        link_directories(${TANGO_OMNI_BASE}/lib/x86_win32/)
-        include_directories(SYSTEM ${TANGO_OMNI_BASE}/include)
+        set(OMNIORB_PKG_LIBRARY_DIRS ${TANGO_OMNI_BASE}/lib/x86_win32)
+        set(OMNIORB_PKG_LIBRARIES_DYN_DEBUG "omniORB4_rtd.lib;omniDynamic4_rtd.lib;omnithread_rtd.lib;COS4_rtd.lib")
+        set(OMNIORB_PKG_LIBRARIES_STA_DEBUG "omniORB4d.lib;omniDynamic4d.lib;omnithreadd.lib;COS4d.lib")
+        set(OMNIORB_PKG_LIBRARIES_DYN_RELEASE "omniORB4_rt.lib;omniDynamic4_rt.lib;omnithread_rt.lib;COS4_rt.lib")
+        set(OMNIORB_PKG_LIBRARIES_STA_RELEASE "omniORB4.lib;omniDynamic4.lib;omnithread.lib;COS4.lib")
         if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-            set(OMNIORB_PKG_LIBRARIES_DYN "omniORB4_rtd.lib;omniDynamic4_rtd.lib;omnithread_rtd.lib;COS4_rtd.lib")
-            set(OMNIORB_PKG_LIBRARIES_STA "omniORB4d.lib;omniDynamic4d.lib;omnithreadd.lib;COS4d.lib")
+            set(OMNIORB_PKG_LIBRARIES_DYN ${OMNIORB_PKG_LIBRARIES_DYN_DEBUG})
+            set(OMNIORB_PKG_LIBRARIES_STA ${OMNIORB_PKG_LIBRARIES_STA_DEBUG})
         else()
-            set(OMNIORB_PKG_LIBRARIES_DYN "omniORB4_rt.lib;omniDynamic4_rt.lib;omnithread_rt.lib;COS4_rt.lib")
-            set(OMNIORB_PKG_LIBRARIES_STA "omniORB4.lib;omniDynamic4.lib;omnithread.lib;COS4.lib")
+            set(OMNIORB_PKG_LIBRARIES_DYN ${OMNIORB_PKG_LIBRARIES_DYN_RELEASE})
+            set(OMNIORB_PKG_LIBRARIES_STA ${OMNIORB_PKG_LIBRARIES_STA_RELEASE})
         endif()
     else()
         set(OMNIIDL_PATH ${TANGO_OMNI_BASE}/bin/)
-        include_directories(SYSTEM ${TANGO_OMNI_BASE}/include)
-        link_directories(${TANGO_OMNI_BASE}/lib)
+        set(OMNIORB_PKG_LIBRARY_DIRS ${TANGO_OMNI_BASE}/lib)
         set(OMNIORB_PKG_LIBRARIES "-lomniORB4 -lomnithread -lCOS4 -lomniDynamic4")
     endif()
+    include_directories(SYSTEM ${OMNIORB_PKG_INCLUDE_DIRS})
+    link_directories(${OMNIORB_PKG_LIBRARY_DIRS})
 else()
     #CORBA
-    pkg_search_module(OMNIORB_PKG REQUIRED omniORB4)
+    pkg_search_module(OMNIORB_PKG REQUIRED omniORB4>=4.2.2)
     #COS
-    pkg_search_module(OMNICOS_PKG REQUIRED omniCOS4)
+    pkg_search_module(OMNICOS_PKG REQUIRED omniCOS4>=4.2.2)
     #dynamic CORBA
-    pkg_search_module(OMNIDYN_PKG REQUIRED omniDynamic4)
+    pkg_search_module(OMNIDYN_PKG REQUIRED omniDynamic4>=4.2.2)
 endif()
+
+include(configure/find_and_check_omniidl.cmake)
 
 if(TANGO_ZMQ_BASE)
     message("Using TANGO_ZMQ_BASE=${TANGO_ZMQ_BASE}")

configure/test_omniidl.cpp
@@ -1,0 +1,20 @@
+#include "test_omniidl.h"
+#include <iostream>
+
+int main() {
+  // omniidl 4.2.1 has a bug in the c++ generation of IDL unions.
+  // The union's copy constructor will not check the _pd__initialised flag
+  // before accessing the other union to do the copy.
+  //
+  // Here we check for this by constructing a zeroed union and trying to copy
+  // it.  The first member (with value 0) of the union is an IDL sequence so the
+  // active member of the zeroed struct is a null pointer.  Therefore, this copy
+  // will result in a segmentation fault.
+
+  TestOmniidl::AttrValUnion zeroed;
+  memset(static_cast<void *>(&zeroed), 0, sizeof(TestOmniidl::AttrValUnion));
+  TestOmniidl::AttrValUnion copy{zeroed};
+
+  // Print something so that the above is not optimized away.
+  std::cout << "Active union memebr: " << static_cast<int>(copy._d()) << "\n";
+}

configure/test_omniidl.idl
@@ -1,0 +1,26 @@
+module TestOmniidl
+{
+
+typedef boolean 				DevBoolean;
+typedef short 					DevShort;
+
+typedef sequence<boolean>		DevVarBooleanArray;
+typedef sequence<short> 		DevVarShortArray;
+
+enum AttributeDataType
+{
+	ATT_BOOL,
+	ATT_SHORT
+};
+
+
+union AttrValUnion switch (AttributeDataType)
+{
+case ATT_BOOL:
+	DevVarBooleanArray		bool_att_value;
+case ATT_SHORT:
+	DevVarShortArray		short_att_value;
+};
+
+
+};

cppapi/include/tango/idl/CMakeLists.txt
@@ -1,8 +1,4 @@
-message("Using OMNIIDL_PATH=${OMNIIDL_PATH}")
 message("Using IDL=${IDL_PKG_INCLUDE_DIRS}")
-
-find_program(OMNIIDL NAMES omniidl
-                     HINTS ${OMNIIDL_PATH})
 
 if(NOT OMNIIDL)
   message(FATAL_ERROR "Could not find omniidl")

commit 6a43ee37d05a2254fcf332ef1642a25f6051d5b2

configure/find_and_check_omniidl.cmake: Prefer CMAKE_CURRENT_SOURCE_DIR
Only that works when cppTango is included into another cmake project.

Bug introduced in 17ec13bf (configure: Check omniorb version and omniidl
code generation, 2022-11-21).

See also 778ea2c0 (configure/CMakeLists.txt: Prefer CMAKE_CURRENT_XXX
variables, 2022-09-05) where we fixed this before.

configure/find_and_check_omniidl.cmake
@@ -7,7 +7,7 @@ function(test_omniidl)
     message(STATUS "Testing omniidl for bug in generated c++ for IDL union")
     execute_process(
         COMMAND ${OMNIIDL} -bcxx -Wbh=.h -Wbs=SK.cpp -Wbd=DynSK.cpp -Wba
-            ${CMAKE_SOURCE_DIR}/configure/test_omniidl.idl
+            ${CMAKE_CURRENT_SOURCE_DIR}/configure/test_omniidl.idl
         WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
         RESULT_VARIABLE OMNIIDL_RESULT
         OUTPUT_VARIABLE OMNIIDL_OUTPUT
@@ -47,7 +47,7 @@ function(test_omniidl)
     try_run(TANGO_OMNIIDL_HAS_NO_UNION_BUG TANGO_OMNIIDL_CHECK_COMPILE
         ${CMAKE_CURRENT_BINARY_DIR}/
         SOURCES
-            ${CMAKE_SOURCE_DIR}/configure/test_omniidl.cpp
+            ${CMAKE_CURRENT_SOURCE_DIR}/configure/test_omniidl.cpp
             ${CMAKE_CURRENT_BINARY_DIR}/test_omniidlSK.cpp
             ${CMAKE_CURRENT_BINARY_DIR}/test_omniidlDynSK.cpp
         CMAKE_FLAGS