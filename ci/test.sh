1bf93359214be60ec778bc473cac8141acc69545
Thomas Braun <thomas.braun@byte-physics.de>
2023-07-19 21:28:10 +0200

    ci/test.sh: Avoid relying on procfs being available if possible
    By using sysctl we avoid relying on procfs. But we only do that if sysctl
    is present as the command is on debian in the package procps and that
    might not be installed.
    
    Helped-by: Thomas Juerges <thomas.juerges@skao.int>

--- ci/test.sh
+++ ci/test.sh
@@ -11,7 +11,13 @@ if [[ "$GITLAB_CI" == "true" ]]
 then
   echo "Preparing for test run inside CI"
   ulimit -c unlimited
-  sudo sh -c 'echo "core.%e.%p.%t" > /proc/sys/kernel/core_pattern'
+
+  if hash sysctl 2>/dev/null
+  then
+    sudo sh -c 'sysctl kernel.core_pattern="core.%e.%p.%t"'
+  else
+    sudo sh -c 'echo "core.%e.%p.%t" > /proc/sys/kernel/core_pattern'
+  fi
 
   docker pull registry.gitlab.com/tango-controls/docker/mysql:5.16-mysql-5
   docker pull registry.gitlab.com/tango-controls/docker/tango-db:5.16-1
