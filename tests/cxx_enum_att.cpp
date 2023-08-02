6dccde584902ad65b3cb238a1576132254f000a4
Thomas Braun <thomas.braun@byte-physics.de>
2023-08-02 19:10:31 +0200

    tests/cxx_enum_att.cpp: Add test for read/write of enumeration values into DeviceAttribute
    The test test_enum_operators_compile_check does only compile with the fix
    from the previous commit.

--- tests/cxx_enum_att.cpp
+++ tests/cxx_enum_att.cpp
@@ -6,6 +6,22 @@
 #undef SUITE_NAME
 #define SUITE_NAME EnumAttTestSuite
 
+enum EnumShort : short
+{
+    EnumShort_A = 0,
+    EnumShort_B = 1,
+    EnumShort_C = 2,
+    EnumShort_D = 3
+};
+
+enum EnumUInt : unsigned int
+{
+    EnumUInt_A = 0,
+    EnumUInt_B = 1,
+    EnumUInt_C = 2,
+    EnumUInt_D = 3
+};
+
 class EnumAttTestSuite : public CxxTest::TestSuite
 {
   protected:
@@ -274,6 +290,90 @@ class EnumAttTestSuite : public CxxTest::TestSuite
         TS_ASSERT_EQUALS(v_sh_write[0], 1);
         TS_ASSERT_EQUALS(v_sh_write[1], 1);
     }
+
+    void test_enum_scalar_attribute_writing_with_enum_type_short()
+    {
+        EnumShort sh_wr = EnumShort_B;
+        DeviceAttribute da_wr;
+        da_wr.set_name("Enum_attr_rw");
+        TS_ASSERT_THROWS_NOTHING(da_wr << sh_wr);
+        TS_ASSERT_THROWS_NOTHING(device1->write_attribute(da_wr));
+
+        DeviceAttribute da_read;
+        TS_ASSERT_THROWS_NOTHING(da_read = device1->read_attribute("Enum_attr_rw"));
+        TS_ASSERT_EQUALS(da_read.get_type(), Tango::DEV_ENUM);
+
+        EnumShort sh_rd;
+        TS_ASSERT_THROWS_NOTHING(da_read >> sh_rd);
+    }
+
+    void test_enum_spectrum_attribute_writing_with_enum_type_short()
+    {
+        std::vector<EnumShort> sh_wr = {EnumShort_A, EnumShort_B, EnumShort_C, EnumShort_D};
+        DeviceAttribute da_wr;
+        da_wr.set_name("Enum_spec_attr_rw");
+        TS_ASSERT_THROWS_NOTHING(da_wr << sh_wr);
+        TS_ASSERT_THROWS_NOTHING(device1->write_attribute(da_wr));
+
+        DeviceAttribute da_read;
+        TS_ASSERT_THROWS_NOTHING(da_read = device1->read_attribute("Enum_spec_attr_rw"));
+        TS_ASSERT_EQUALS(da_read.get_type(), Tango::DEV_ENUM);
+
+        std::vector<EnumShort> sh_rd;
+        TS_ASSERT_THROWS_NOTHING(da_read >> sh_rd);
+    }
+
+    void test_enum_scalar_attribute_writing_with_enum_type_uint()
+    {
+        EnumUInt int_wr = EnumUInt_B;
+        DeviceAttribute da_wr;
+        da_wr.set_name("Enum_attr_rw");
+        TS_ASSERT_THROWS_NOTHING(da_wr << int_wr);
+        TS_ASSERT_THROWS_NOTHING(device1->write_attribute(da_wr));
+
+        DeviceAttribute da_read;
+        TS_ASSERT_THROWS_NOTHING(da_read = device1->read_attribute("Enum_attr_rw"));
+        TS_ASSERT_EQUALS(da_read.get_type(), Tango::DEV_ENUM);
+
+        EnumUInt int_rd;
+        TS_ASSERT_THROWS_NOTHING(da_read >> int_rd);
+    }
+
+    void test_enum_spectrum_attribute_writing_with_enum_type_uint()
+    {
+        std::vector<EnumUInt> int_wr = {EnumUInt_A, EnumUInt_B, EnumUInt_C, EnumUInt_D};
+        DeviceAttribute da_wr;
+        da_wr.set_name("Enum_spec_attr_rw");
+        TS_ASSERT_THROWS_NOTHING(da_wr << int_wr);
+        TS_ASSERT_EQUALS(da_wr.get_type(), Tango::DEV_ENUM);
+        TS_ASSERT_THROWS_NOTHING(device1->write_attribute(da_wr));
+
+        DeviceAttribute da_read;
+        TS_ASSERT_THROWS_NOTHING(da_read = device1->read_attribute("Enum_spec_attr_rw"));
+        TS_ASSERT_EQUALS(da_read.get_type(), Tango::DEV_ENUM);
+
+        std::vector<EnumUInt> int_rd;
+        TS_ASSERT_THROWS_NOTHING(da_read >> int_rd);
+    }
+
+    void test_enum_operators_compile_check_our_types()
+    {
+        DeviceAttribute da_ll;
+        DevLong64 ll = 0;
+        TS_ASSERT_THROWS_NOTHING(da_ll << ll);
+
+        DeviceAttribute da_ull;
+        DevULong64 ull = 0;
+        TS_ASSERT_THROWS_NOTHING(da_ull << ull);
+    }
+
+    // can't test compile failures
+    // void test_enum_operators_compile_check_fails()
+    // {
+    //     EnumUInt int_wr = EnumUInt_B;
+    //     DeviceAttribute da_wr;
+    //     TS_ASSERT_THROWS_NOTHING(da_wr << int_wr);
+    // }
 
     void test_enum_attribute_write_read()
     {
