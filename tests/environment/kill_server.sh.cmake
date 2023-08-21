420f630e1ddf90520dbb6f856554bab5555ebcb5
Thomas Ives <tri@observatorysciences.co.uk>
2023-08-21 14:15:18 +0100

    kill_server.sh: Avoid non-standard --timeout option for kill command
    On some systems, kill does not support the --timeout option, making this
    script not portable.  We replace the --timeout usage with a more
    portable implementation of the same behaviour in bash.

--- tests/environment/kill_server.sh.cmake
+++ tests/environment/kill_server.sh.cmake
@@ -5,7 +5,21 @@ export SHELLOPTS
 set -o errexit
 set -o nounset
 
-pids=$(tr '\n' ' ' <"${TANGO_TEST_CASE_DIRECTORY}/server_pids")
+export pids=$(tr '\n' ' ' <"${TANGO_TEST_CASE_DIRECTORY}/server_pids")
+
+function wait_pids() {
+    local dead=
+    while [[ $(echo $pids | wc -w) -gt $(echo $dead | wc -w) ]]; do
+        dead=
+        for pid in $pids; do
+            if ! ps --pid $pid &>/dev/null; then
+                dead="$dead $pid"
+            fi
+        done
+        sleep 0.1s
+    done
+}
+export -f wait_pids
 
 if [[ -z "$pids" ]]; then
     echo "No device servers to kill"
@@ -13,4 +27,8 @@ if [[ -z "$pids" ]]; then
 fi
 
 echo "Killing PIDs: $pids"
-kill -s TERM --timeout 5000 KILL -- $pids &>/dev/null
+kill -s TERM $pids
+timeout 5s bash -c wait_pids
+if [[ $? -eq 124 ]]; then
+    kill -s KILL $pids
+fi
