982643bf3302bc79272249d034382f440f22e413
Thomas Braun <thomas.braun@byte-physics.de>
2023-11-16 19:59:40 +0100

    CMake: Remove setting arbitrary CMAKE_CXX_FLAGS
    It is not clear why we would want to set these special flags for the
    release type. So let's just remove them.

--- ci/config.sh
+++ ci/config.sh
@@ -65,7 +65,6 @@ cmake                                                                    \
   -DCMAKE_VERBOSE_MAKEFILE=ON                                            \
   -Dcppzmq_ROOT=${INSTALL_DIR}                                           \
   -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}                                 \
-  -DCMAKE_CXX_FLAGS_RELEASE="-g -O3"                                     \
   -DCMAKE_DISABLE_PRECOMPILE_HEADERS=${CMAKE_DISABLE_PRECOMPILE_HEADERS} \
   -DTANGO_USE_JPEG=${TANGO_USE_JPEG}                                     \
   -DTANGO_USE_LIBCPP=${TANGO_USE_LIBCPP}                                 \
