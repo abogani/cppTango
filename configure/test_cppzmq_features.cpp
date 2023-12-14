da9f548e4ec37c39f09e63aba0e1077faac0b2e9
Thomas Braun <thomas.braun@byte-physics.de>
2023-12-14 19:39:49 +0100

    configure/test_cppzmq_features.cpp: Prefer standard C++17 attributes
    This is possible since e2013bc7 (CMakeLists.txt: Move to C++17,
    2023-09-28) and allows us to use the compile test also on Windows.

--- configure/test_cppzmq_features.cpp
+++ configure/test_cppzmq_features.cpp
@@ -4,18 +4,18 @@ int main(int, char**)
 {
   zmq::context_t c(1);
   zmq::socket_t s(c, ZMQ_REQ);
-  void* plain_socket __attribute__((unused)) = s.handle();
+  void* plain_socket [[maybe_unused]] = s.handle();
 
   s.disconnect("some endpoint");
 
   zmq::message_t m, n;
-  zmq::send_result_t sr __attribute__((unused)) = s.send(m, zmq::send_flags::none);
-  zmq::recv_result_t rr __attribute__((unused)) = s.recv(m, zmq::recv_flags::none);
+  zmq::send_result_t sr [[maybe_unused]] = s.send(m, zmq::send_flags::none);
+  zmq::recv_result_t rr [[maybe_unused]] = s.recv(m, zmq::recv_flags::none);
 
   m.copy(n);
   m.move(n);
 
   s.set(zmq::sockopt::linger, 0);
-  int val __attribute__((unused)) = s.get(zmq::sockopt::linger);
+  int val [[maybe_unused]] = s.get(zmq::sockopt::linger);
   return 0;
 }
