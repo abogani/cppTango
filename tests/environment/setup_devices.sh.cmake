35b60096d61dba036300de15dfdf795e746b5788
Thomas Ives <tri@observatorysciences.co.uk>
2023-12-18 15:18:37 +0000

    tests/CMakeLists.txt: Add tests running against IDL5 dservers

--- tests/environment/setup_devices.sh.cmake
+++ tests/environment/setup_devices.sh.cmake
@@ -6,9 +6,9 @@ set -e
 export TANGO_TEST_CASE_DIRECTORY="${TANGO_TEST_CASE_DIRECTORY:-.}"
 
 "@PROJECT_BINARY_DIR@/tests/conf_devtest" \
-    @SERV_NAME@/@INST_NAME@ \
-    @SERV_NAME@/@INST_NAME2@ \
-    @FWD_SERV_NAME@/@INST_NAME@ \
+    @SERV_NAME@${IDL_SUFFIX}/@INST_NAME@ \
+    @SERV_NAME@${IDL_SUFFIX}/@INST_NAME2@ \
+    @FWD_SERV_NAME@${IDL_SUFFIX}/@INST_NAME@ \
     @DEV1@ \
     @DEV2@ \
     @DEV3@ \
@@ -20,10 +20,10 @@ export TANGO_TEST_CASE_DIRECTORY="${TANGO_TEST_CASE_DIRECTORY:-.}"
 
 # Bug reported in cppTango#1022 occurs when there are several devices defined for the same device server instance
 # We define 2 devices:
-"@CMAKE_CURRENT_BINARY_DIR@/tango_admin.sh" --add-server TestCppTango1022/@INST_NAME@ TestCppTango1022 @TEST_CPPTANGO_1022_DEV1@,@TEST_CPPTANGO_1022_DEV2@
+"@CMAKE_CURRENT_BINARY_DIR@/tango_admin.sh" --add-server TestCppTango1022${IDL_SUFFIX}/@INST_NAME@ TestCppTango1022 @TEST_CPPTANGO_1022_DEV1@,@TEST_CPPTANGO_1022_DEV2@
 
-"@CMAKE_CURRENT_BINARY_DIR@/start_server.sh" @INST_NAME@ DevTest device_server/generic
+"@CMAKE_CURRENT_BINARY_DIR@/start_server.sh" @INST_NAME@ DevTest${IDL_SUFFIX} device_server/generic
 
-"@CMAKE_CURRENT_BINARY_DIR@/start_server.sh" @INST_NAME@ FwdTest device_server/forward
+"@CMAKE_CURRENT_BINARY_DIR@/start_server.sh" @INST_NAME@ FwdTest${IDL_SUFFIX} device_server/forward
 
-"@CMAKE_CURRENT_BINARY_DIR@/start_server.sh" @INST_NAME@ TestCppTango1022 device_server/TestCppTango1022
+"@CMAKE_CURRENT_BINARY_DIR@/start_server.sh" @INST_NAME@ TestCppTango1022${IDL_SUFFIX} device_server/TestCppTango1022
