abogani/cppTango

commit 6db810cab9b824fdd7b7b33b2fbc45a38dbf8756

Add Device_6Impl

src/include/tango/server/device_6.h
@@ -1,0 +1,184 @@
+//===================================================================================================================
+//
+// file :		Device_6.h
+//
+// description :	Include for the Device root classes in its release 5 (IDL release 5, Tango release 9)
+//
+// project :		TANGO
+//
+// author(s) :		E.Taurel
+//
+// Copyright (C) :      2014,2015
+//						European Synchrotron Radiation Facility
+//                      BP 220, Grenoble 38043
+//                      FRANCE
+//
+// This file is part of Tango.
+//
+// Tango is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Tango is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with Tango.
+// If not, see <http://www.gnu.org/licenses/>.
+//
+//
+//
+//===================================================================================================================
+
+#ifndef _DEVICE_6_H
+#define _DEVICE_6_H
+
+#include <tango/tango.h>
+
+namespace Tango
+{
+
+class WPipe;
+
+//==================================================================================================================
+//
+//			The Device_6Impl class
+//
+//
+// description :
+// 		This class is derived directly from the Tango::Device_skel class generated by CORBA. It represents the
+// CORBA
+//		servant which will be accessed by the client. It implements all the methods and attributes defined in
+// the
+// 		IDL interface for Device.
+//
+//==================================================================================================================
+
+/**
+ * Base class for all TANGO device since version 5.
+ *
+ * This class inherits from DeviceImpl class which itself inherits from
+ * CORBA classes where all the network layer is implemented.
+ * This class has been created since release 7 of Tango library where the IDL
+ * Tango module has been modified in order to create a Device_5 interface
+ * which inherits from the original Device interface
+ *
+ *
+ *
+ *
+ * @headerfile tango.h
+ * @ingroup Server
+ */
+
+// NOLINTNEXTLINE(readability-identifier-naming)
+class Device_6Impl : public virtual POA_Tango::Device_6, public Device_5Impl
+{
+  public:
+    /**@name Constructors
+     * Miscellaneous constructors */
+    //@{
+    /**
+     * Constructs a newly allocated Device_6Impl object from its name.
+     *
+     * The device description field is set to <i>A Tango device</i>. The device
+     * state is set to unknown and the device status is set to
+     * <b>Not Initialised</b>
+     *
+     * @param 	device_class	Pointer to the device class object
+     * @param	dev_name	The device name
+     *
+     */
+    Device_6Impl(DeviceClass *device_class, const std::string &dev_name);
+
+    /**
+     * Constructs a newly allocated Device_6Impl object from its name and its description.
+     *
+     * The device
+     * state is set to unknown and the device status is set to
+     * <i>Not Initialised</i>
+     *
+     * @param 	device_class	Pointer to the device class object
+     * @param	dev_name	The device name
+     * @param	desc	The device description
+     *
+     */
+    Device_6Impl(DeviceClass *device_class, const std::string &dev_name, const std::string &desc);
+
+    /**
+     * Constructs a newly allocated Device_6Impl object from all its creation
+     * parameters.
+     *
+     * The device is constructed from its name, its description, an original state
+     * and status
+     *
+     * @param 	device_class	Pointer to the device class object
+     * @param	dev_name	The device name
+     * @param	desc 		The device description
+     * @param	dev_state 	The device initial state
+     * @param	dev_status	The device initial status
+     *
+     */
+    Device_6Impl(DeviceClass *device_class,
+                 const std::string &dev_name,
+                 const std::string &desc,
+                 Tango::DevState dev_state,
+                 const std::string &dev_status);
+
+    /**
+     * Constructs a newly allocated Device_6Impl object from all its creation
+     * parameters with some default values.
+     *
+     * The device is constructed from its name, its description, an original state
+     * and status. This constructor defined default values for the description,
+     * state and status parameters. The default device description is <i>A TANGO device</i>.
+     * The default device state is <i>UNKNOWN</i> and the default device status
+     * is <i>Not initialised</i>.
+     *
+     * @param 	device_class	Pointer to the device class object
+     * @param	dev_name	The device name
+     * @param	desc	The device desc
+     * @param	dev_state 	The device initial state
+     * @param	dev_status	The device initial status
+     *
+     */
+    Device_6Impl(DeviceClass *device_class,
+                 const char *dev_name,
+                 const char *desc = "A TANGO device",
+                 Tango::DevState dev_state = Tango::UNKNOWN,
+                 const char *dev_status = StatusNotSet);
+
+    //@}
+
+    /**@name Destructor
+     * Only one desctructor is defined for this class */
+    //@{
+    /**
+     * The device desctructor.
+     */
+    virtual ~Device_6Impl() { }
+
+    //@}
+
+    /**@name CORBA operation methods
+     * Method defined to implement TANGO device CORBA operation */
+    //@{
+
+    //@}
+
+    /// @privatesection
+
+  private:
+    class Device_6ImplExt
+    {
+      public:
+        Device_6ImplExt() { }
+
+        ~Device_6ImplExt() { }
+    };
+
+    std::unique_ptr<Device_6ImplExt> ext_6; // Class extension
+};
+
+} // namespace Tango
+
+#endif // _DEVICE_6_H

src/client/devapi_base.cpp
@@ -203,6 +203,8 @@ Connection::Connection(const Connection &sou) :
 
     device_5 = sou.device_5;
 
+    device_6 = sou.device_6;
+
     if(sou.ext != nullptr)
     {
         ext = std::make_unique<ConnectionExt>();
@@ -426,70 +428,85 @@ void Connection::connect(const std::string &corba_name)
                 }
             }
 
-            device_5 = Device_5::_narrow(obj);
+            device_6 = Device_6::_narrow(obj);
 
-            if(CORBA::is_nil(device_5))
+            if(CORBA::is_nil(device_6))
             {
-                device_4 = Device_4::_narrow(obj);
+                device_5 = Device_5::_narrow(obj);
 
-                if(CORBA::is_nil(device_4))
+                if(CORBA::is_nil(device_5))
                 {
-                    device_3 = Device_3::_narrow(obj);
+                    device_4 = Device_4::_narrow(obj);
 
-                    if(CORBA::is_nil(device_3))
+                    if(CORBA::is_nil(device_4))
                     {
-                        device_2 = Device_2::_narrow(obj);
-                        if(CORBA::is_nil(device_2))
-                        {
-                            device = Device::_narrow(obj);
-                            if(CORBA::is_nil(device))
-                            {
-                                std::cerr << "Can't build connection to object " << corba_name << std::endl;
-                                connection_state = CONNECTION_NOTOK;
+                        device_3 = Device_3::_narrow(obj);
 
-                                TangoSys_OMemStream desc;
-                                desc << "Failed to connect to device " << dev_name();
-                                desc << " (device nil after _narrowing)" << std::ends;
-                                TANGO_THROW_API_EXCEPTION(ApiConnExcept, API_CantConnectToDevice, desc.str());
+                        if(CORBA::is_nil(device_3))
+                        {
+                            device_2 = Device_2::_narrow(obj);
+                            if(CORBA::is_nil(device_2))
+                            {
+                                device = Device::_narrow(obj);
+                                if(CORBA::is_nil(device))
+                                {
+                                    std::cerr << "Can't build connection to object " << corba_name << std::endl;
+                                    connection_state = CONNECTION_NOTOK;
+
+                                    TangoSys_OMemStream desc;
+                                    desc << "Failed to connect to device " << dev_name();
+                                    desc << " (device nil after _narrowing)" << std::ends;
+                                    TANGO_THROW_API_EXCEPTION(ApiConnExcept, API_CantConnectToDevice, desc.str());
+                                }
+                                else
+                                {
+                                    device->_non_existent();
+                                    version = 1;
+                                }
                             }
                             else
                             {
-                                device->_non_existent();
-                                version = 1;
+                                device_2->_non_existent();
+                                version = 2;
+                                device = Device_2::_duplicate(device_2);
                             }
                         }
                         else
                         {
-                            device_2->_non_existent();
-                            version = 2;
-                            device = Device_2::_duplicate(device_2);
+                            device_3->_non_existent();
+                            version = 3;
+                            device_2 = Device_3::_duplicate(device_3);
+                            device = Device_3::_duplicate(device_3);
                         }
                     }
                     else
                     {
-                        device_3->_non_existent();
-                        version = 3;
-                        device_2 = Device_3::_duplicate(device_3);
-                        device = Device_3::_duplicate(device_3);
+                        device_4->_non_existent();
+                        version = 4;
+                        device_3 = Device_4::_duplicate(device_4);
+                        device_2 = Device_4::_duplicate(device_4);
+                        device = Device_4::_duplicate(device_4);
                     }
                 }
                 else
                 {
-                    device_4->_non_existent();
-                    version = 4;
-                    device_3 = Device_4::_duplicate(device_4);
-                    device_2 = Device_4::_duplicate(device_4);
-                    device = Device_4::_duplicate(device_4);
+                    device_5->_non_existent();
+                    version = 5;
+                    device_4 = Device_5::_duplicate(device_5);
+                    device_3 = Device_5::_duplicate(device_5);
+                    device_2 = Device_5::_duplicate(device_5);
+                    device = Device_5::_duplicate(device_5);
                 }
             }
             else
             {
-                device_5->_non_existent();
-                version = 5;
-                device_4 = Device_5::_duplicate(device_5);
-                device_3 = Device_5::_duplicate(device_5);
-                device_2 = Device_5::_duplicate(device_5);
-                device = Device_5::_duplicate(device_5);
+                device_6->_non_existent();
+                version = 6;
+                device_5 = Device_6::_duplicate(device_6);
+                device_4 = Device_6::_duplicate(device_6);
+                device_3 = Device_6::_duplicate(device_6);
+                device_2 = Device_6::_duplicate(device_6);
+                device = Device_6::_duplicate(device_6);
             }
 
             //
@@ -3905,6 +3922,7 @@ AttributeInfoListEx *DeviceProxy::get_attribute_config_ex(const std::vector<std:
             break;
 
             case 5:
+            case 6:
             {
                 Device_5_var dev = Device_5::_duplicate(device_5);
                 attr_config_list_5 = dev->get_attribute_config_5(attr_list);
@@ -5422,7 +5440,7 @@ std::vector<DeviceAttribute> *DeviceProxy::read_attributes(const std::vector<std
     {
         if(version >= 3)
         {
-            if(version == 5)
+            if(version >= 5)
             {
                 ApiUtil::attr_to_device(&(attr_value_list_5[i]), version, &(*dev_attr)[i]);
             }
@@ -9576,6 +9594,7 @@ int DeviceProxy::get_tango_lib_version()
     }
 
     case 5:
+    case 6:
         ret = 902;
         break;
 

src/include/tango/client/Connection.h
@@ -138,6 +138,8 @@ class Connection
 
     Tango::Device_5_var device_5;
 
+    Tango::Device_6_var device_6;
+
   public:
     ///@publicsection
     /**@name Miscellaneous methods */

src/include/tango/common/tango_const.h
@@ -42,7 +42,7 @@ namespace Tango
 // Some general interest define
 //
 
-#define TANGO_BASE_CLASS Tango::Device_5Impl
+#define TANGO_BASE_CLASS Tango::Device_6Impl
 
 #define TBS(s) #s
 #define XTBS(s) TBS(s)
@@ -53,7 +53,7 @@ const char *const TgLibMajorVers = XTBS(TANGO_VERSION_MAJOR);
 
 const int TgLibVersNb = TANGO_VERSION_MAJOR * 10000 + TANGO_VERSION_MINOR * 100 + TANGO_VERSION_PATCH;
 
-const int DevVersion = 5; // IDL version number
+const int DevVersion = 6; // IDL version number
 const int DefaultMaxSeq = 20;
 const int DefaultBlackBoxDepth = 50;
 const int DefaultPollRingDepth = 10;

src/include/tango/server/CMakeLists.txt
@@ -21,6 +21,7 @@ set(SERVER_HEADERS attrdesc.h
     device_3_templ.h
     device_4.h
     device_5.h
+    device_6.h
     deviceclass.h
     devintr.h
     dintrthread.h

src/include/tango/tango.h
@@ -156,6 +156,7 @@ inline void string_free(char *s)
   #include <tango/server/device_3.h>
   #include <tango/server/device_4.h>
   #include <tango/server/device_5.h>
+  #include <tango/server/device_6.h>
   #include <tango/server/command.h>
   #include <tango/server/pipedesc.h>
   #include <tango/server/pipe.h>

src/server/CMakeLists.txt
@@ -18,6 +18,7 @@ set(SOURCES attrdesc.cpp
             device_3.cpp
             device_4.cpp
             device_5.cpp
+            device_6.cpp
             deviceclass.cpp
             devicelog.cpp
             devintr.cpp

src/server/device_6.cpp
@@ -1,0 +1,113 @@
+//+===================================================================================================================
+//
+// file :               Device_6.cpp
+//
+// description :        C++ source code for the Device_6Impl class. This class is the root class for all derived Device
+//						classes starting with Tango 9
+//
+// project :            TANGO
+//
+// author(s) :          E.Taurel
+//
+// Copyright (C) :      2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015
+//						European Synchrotron Radiation Facility
+//                      BP 220, Grenoble 38043
+//                      FRANCE
+//
+// This file is part of Tango.
+//
+// Tango is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Tango is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with Tango.
+// If not, see <http://www.gnu.org/licenses/>.
+//
+//
+//
+//-===================================================================================================================
+
+#if defined(__GNUC__) && !defined(__clang__)
+  #pragma GCC diagnostic push
+  #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+  #pragma GCC diagnostic ignored "-Wunused-parameter"
+#endif
+
+#if defined(__clang__)
+  #pragma clang diagnostic push
+  #pragma clang diagnostic ignored "-Wsometimes-uninitialized"
+  #pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+#include <tango/tango.h>
+
+#if defined(__clang__)
+  #pragma clang diagnostic pop
+#endif
+
+#if defined(__GNUC__) && !defined(__clang__)
+  #pragma GCC diagnostic pop
+#endif
+
+#include <tango/server/device_6.h>
+#include <tango/server/eventsupplier.h>
+#include <tango/server/device_3_templ.h>
+
+namespace Tango
+{
+
+//+-------------------------------------------------------------------------------------------------------------------
+//
+// method :
+//		Device_6Impl::Device_6Impl
+//
+// description :
+//		Constructors for the Device_6impl class from the class object pointer, the device name, the description
+// field,
+// 		the state and the status. Device_6Impl inherits from DeviceImpl. These constructors simply call the
+// correct
+//		DeviceImpl class constructor
+//
+//--------------------------------------------------------------------------------------------------------------------
+
+Device_6Impl::Device_6Impl(DeviceClass *device_class, const std::string &dev_name) :
+    Device_5Impl(device_class, dev_name),
+    ext_6(nullptr)
+{
+    idl_version = 6;
+}
+
+Device_6Impl::Device_6Impl(DeviceClass *device_class, const std::string &dev_name, const std::string &desc) :
+    Device_5Impl(device_class, dev_name, desc),
+    ext_6(nullptr)
+{
+    idl_version = 6;
+}
+
+Device_6Impl::Device_6Impl(DeviceClass *device_class,
+                           const std::string &dev_name,
+                           const std::string &desc,
+                           Tango::DevState dev_state,
+                           const std::string &dev_status) :
+    Device_5Impl(device_class, dev_name, desc, dev_state, dev_status),
+    ext_6(nullptr)
+{
+    idl_version = 6;
+}
+
+Device_6Impl::Device_6Impl(DeviceClass *device_class,
+                           const char *dev_name,
+                           const char *desc,
+                           Tango::DevState dev_state,
+                           const char *dev_status) :
+    Device_5Impl(device_class, dev_name, desc, dev_state, dev_status),
+    ext_6(nullptr)
+{
+    idl_version = 6;
+}
+
+} // namespace Tango

commit 8c6d7952bf5aa11f8049dcbb2bf3d01666e08d1c

Device_6Impl: Prefer override to virtual
This satisfies the modernize-use-override clang-tidy check.  The check
was auto-fixed in 7af327f2 (clang-tidy: Auto-fix modernize-use-override,
2023-09-01) and has been failing since 6db810ca (Add Device_6Impl,
2023-08-18).

The regression was missed because the clang-tidies warnings-as-errors
was broken in 9a7eb485 (.clang-tidy: Suppress failing warnings,
2023-09-18).

src/include/tango/server/device_6.h
@@ -153,7 +153,7 @@ class Device_6Impl : public virtual POA_Tango::Device_6, public Device_5Impl
     /**
      * The device desctructor.
      */
-    virtual ~Device_6Impl() { }
+    ~Device_6Impl() override { }
 
     //@}
 