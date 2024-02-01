abogani/cppTango

commit c3a65891e6ff7522197ad7a98617ce7d467d5047

add support for new IDL6 client identification data struct in the client API - #1193

src/server/blackbox.cpp
@@ -383,9 +383,14 @@ void BlackBox::insert_cmd_cl_ident(const char *cmd, const ClntIdent &cl_id, long
 void BlackBox::add_cl_ident(const ClntIdent &cl_ident, client_addr *cl_addr)
 {
     cl_addr->client_ident = true;
+
     Tango::LockerLanguage cl_lang = cl_ident._d();
+
     cl_addr->client_lang = cl_lang;
-    if(cl_lang == Tango::CPP)
+
+    switch(cl_lang)
+    {
+    case Tango::CPP:
     {
         cl_addr->client_pid = cl_ident.cpp_clnt();
         std::string str(cl_addr->client_ip);
@@ -398,12 +403,46 @@ void BlackBox::add_cl_ident(const ClntIdent &cl_ident, client_addr *cl_addr)
             }
         }
     }
-    else
+    break;
+    case Tango::CPP_6:
+    {
+        Tango::CppClntIdent_6 eci = cl_ident.cpp_clnt_6();
+        cl_addr->client_pid = eci.cpp_clnt;
+        std::string str(cl_addr->client_ip);
+        if(str.find(":unix:") != std::string::npos)
+        {
+            std::string::size_type pos = str.find(' ');
+            if(pos != std::string::npos)
+            {
+                cl_addr->client_ip[pos] = '\0';
+            }
+        }
+    }
+    break;
+    case Tango::JAVA:
     {
         Tango::JavaClntIdent jci = cl_ident.java_clnt();
         cl_addr->java_main_class = jci.MainClass;
         cl_addr->java_ident[0] = jci.uuid[0];
         cl_addr->java_ident[1] = jci.uuid[1];
+    }
+    break;
+    case Tango::JAVA_6:
+    {
+        Tango::JavaClntIdent_6 eci = cl_ident.java_clnt_6();
+        Tango::JavaClntIdent jci = eci.java_clnt;
+        cl_addr->java_main_class = jci.MainClass;
+        cl_addr->java_ident[0] = jci.uuid[0];
+        cl_addr->java_ident[1] = jci.uuid[1];
+    }
+    break;
+    default:
+    {
+        cl_addr->java_main_class = "undefined";
+        cl_addr->java_ident[0] = 0;
+        cl_addr->java_ident[1] = 0;
+    }
+    break;
     }
 }
 
@@ -1622,7 +1661,7 @@ void BlackBox::build_info_as_str(long index)
 
         if(box[index].client_ident == true)
         {
-            if(box[index].client_lang == Tango::CPP)
+            if(box[index].client_lang == Tango::CPP || box[index].client_lang == Tango::CPP_6)
             {
                 elt_str = elt_str + " (CPP/Python client with PID ";
                 TangoSys_MemStream o;
@@ -1647,7 +1686,7 @@ void BlackBox::build_info_as_str(long index)
 
         if(box[index].client_ident == true)
         {
-            if(box[index].client_lang == Tango::CPP)
+            if(box[index].client_lang == Tango::CPP || box[index].client_lang == Tango::CPP_6)
             {
                 elt_str = elt_str + " (CPP/Python client with PID ";
                 TangoSys_MemStream o;
@@ -1919,7 +1958,7 @@ bool client_addr::operator==(const client_addr &rhs)
     }
     else
     {
-        if(client_lang == Tango::CPP)
+        if(client_lang == Tango::CPP || client_lang == Tango::CPP_6)
         {
             if(client_pid != rhs.client_pid)
             {
@@ -1978,7 +2017,7 @@ bool client_addr::operator!=(const client_addr &rhs)
     }
     else
     {
-        if(client_lang == Tango::CPP)
+        if(client_lang == Tango::CPP || client_lang == Tango::CPP_6)
         {
             if(client_pid != rhs.client_pid)
             {
@@ -2133,7 +2172,7 @@ std::ostream &operator<<(std::ostream &o_str, const client_addr &ca)
     }
     else
     {
-        if(ca.client_lang == Tango::CPP)
+        if(ca.client_lang == Tango::CPP || ca.client_lang == Tango::CPP_6)
         {
             std::string cl_name;
             o_str << "CPP or Python client with PID " << ca.client_pid << " from host ";

src/client/dbapi_base.cpp
@@ -641,16 +641,19 @@ void Database::reread_filedatabase()
 
 void Database::build_connection()
 {
-    std::string corba_name = get_corba_name(true);
-    //    omniORB::setClientConnectTimeout(CORBA::ULong(DB_CONNECT_TIMEOUT));
+    // omniORB::setClientConnectTimeout(CORBA::ULong(DB_CONNECT_TIMEOUT));
     try
     {
-        connect(corba_name);
-        //        omniORB::setClientConnectTimeout(0);
+        // we now use reconnect instead of connect
+        // it allows us to know more about the device we are talking to
+        // see Connection::reconnect for details
+        reconnect(true);
+
+        // omniORB::setClientConnectTimeout(0);
     }
     catch(Tango::DevFailed &)
     {
-        //        omniORB::setClientConnectTimeout(0);
+        // omniORB::setClientConnectTimeout(0);
         throw;
     }
 }
@@ -4352,14 +4355,10 @@ CORBA::Any *Database::fill_server_cache(const std::string &ds_name, const std::s
 
     try
     {
-        ClntIdent ci;
-        ApiUtil *au = ApiUtil::instance();
-        ci.cpp_clnt(au->get_client_pid());
-
         if(version >= 5)
         {
             Device_5_var dev = Device_5::_duplicate(device_5);
-            attr_value_list_5 = dev->read_attributes_5(attr_list, DEV, ci);
+            attr_value_list_5 = dev->read_attributes_5(attr_list, DEV, get_client_identification());
 
             ApiUtil::attr_to_device(&((*attr_value_list_5)[0]), version, &da);
             delete attr_value_list_5;
@@ -4367,7 +4366,7 @@ CORBA::Any *Database::fill_server_cache(const std::string &ds_name, const std::s
         else if(version == 4)
         {
             Device_4_var dev = Device_4::_duplicate(device_4);
-            attr_value_list_4 = dev->read_attributes_4(attr_list, DEV, ci);
+            attr_value_list_4 = dev->read_attributes_4(attr_list, DEV, get_client_identification());
 
             ApiUtil::attr_to_device(&((*attr_value_list_4)[0]), version, &da);
             delete attr_value_list_4;

src/client/devapi_base.cpp
@@ -83,13 +83,15 @@ Connection::Connection(CORBA::ORB_var orb_in) :
     pasyn_ctr(0),
     pasyn_cb_ctr(0),
     timeout(CLNT_TIMEOUT),
+    connection_state(CONNECTION_NOTOK),
     version(detail::INVALID_IDL_VERSION),
+    server_version(detail::INVALID_IDL_VERSION),
     source(Tango::CACHE_DEV),
     ext(new ConnectionExt()),
     tr_reco(true),
-
     user_connect_timeout(-1),
     tango_host_localhost(false)
+
 {
     //
     // Some default init for access control
@@ -186,6 +188,7 @@ Connection::Connection(const Connection &sou) :
     timeout = sou.timeout;
     connection_state = sou.connection_state;
     version = sou.version;
+    server_version = sou.server_version;
     source = sou.source;
 
     check_acc = sou.check_acc;
@@ -243,6 +246,7 @@ Connection &Connection::operator=(const Connection &rval)
     timeout = rval.timeout;
     connection_state = rval.connection_state;
     version = rval.version;
+    server_version = rval.server_version;
     source = rval.source;
 
     check_acc = rval.check_acc;
@@ -775,7 +779,26 @@ void Connection::reconnect(bool db_used)
                 //                    omniORB::setClientConnectTimeout(user_connect_timeout);
                 //                else
                 //                    omniORB::setClientConnectTimeout(NARROW_CLNT_TIMEOUT);
-                device->ping();
+
+                //
+                // Impl. change for Tango-10 + IDLv6 - see cppTango #1193
+                //
+                // we now call 'info' instead of 'ping' so that we can obtain the so called
+                // 'server_version' - i.e., the ultimate version supported by the server in
+                // which the device we are connected to is running.
+                //
+                // in some particular cases, this allows us to offer new features to devices
+                // simply recompiled against the latest version of tango without modifing
+                // their inheritance schema.
+                //
+                // the telemetry service is an example of such a case - devices inheriting
+                // from Device_4Impl or Device5_impl will be able to propagate the trace
+                // context as far as they are running within a server where idl 6 or above is
+                // supported.
+                //
+                std::unique_ptr<Tango::DevInfo> device_info(device->info());
+                server_version = device_info->server_version;
+
                 //                omniORB::setClientConnectTimeout(0);
 
                 prev_failed_t0 = tango_nullopt;
@@ -810,6 +833,52 @@ void Connection::reconnect(bool db_used)
 
         throw;
     }
+}
+
+//+----------------------------------------------------------------------------------------------------------------
+//
+// function:
+//
+// 		ClntIdent Connection::get_client_identification() const
+//
+// description:
+//
+//      Returns a ClntIdent initialized according to the IDL version of the device (peer) and the ultimate
+//      IDL version supported by the server in which the peer is running.
+//
+//      Added for the telemetry service introduced in IDLv6.
+//
+//      Any device with IDL version >= 4 running in a server in which the ultimate version of the IDL is >= 6,
+//      will benefit from the telemetry features offered at the kernel level (low level profiling). This
+//      will limit the discontinuity in the tracing information.
+//
+//      See ClntIdent in tango.idl v6 for details.
+//
+//-----------------------------------------------------------------------------------------------------------------
+ClntIdent Connection::get_client_identification() const
+{
+    // the client identification struct to be returned
+    ClntIdent ci;
+
+    // the pid of the cpp server (acting as a client) or the pure cpp client within which this code is executed
+    TangoSys_Pid pid = ApiUtil::instance()->get_client_pid();
+
+    if(version >= 4 && server_version >= 6)
+    {
+        // IDLv6 case
+        CppClntIdent_6 ci_v6;
+        ci_v6.cpp_clnt = pid;
+        auto trace_context{W3CTraceContextV0()};
+        ci_v6.trace_context.data(trace_context);
+        ci.cpp_clnt_6(ci_v6);
+    }
+    else
+    {
+        // pre-IDLv6 case (the pid is the only info set for a cpp client)
+        ci.cpp_clnt(pid);
+    }
+
+    return ci;
 }
 
 //-----------------------------------------------------------------------------
@@ -1309,12 +1378,9 @@ DeviceData Connection::command_inout(const std::string &command, const DeviceDat
             CORBA::Any *received;
             if(version >= 4)
             {
-                ClntIdent ci;
-                ApiUtil *au = ApiUtil::instance();
-                ci.cpp_clnt(au->get_client_pid());
-
                 Device_4_var dev = Device_4::_duplicate(device_4);
-                received = dev->command_inout_4(command.c_str(), data_in.any, local_source, ci);
+                received =
+                    dev->command_inout_4(command.c_str(), data_in.any, local_source, get_client_identification());
             }
             else if(version >= 2)
             {
@@ -1508,12 +1574,8 @@ CORBA::Any_var Connection::command_inout(const std::string &command, const CORBA
 
             if(version >= 4)
             {
-                ClntIdent ci;
-                ApiUtil *au = ApiUtil::instance();
-                ci.cpp_clnt(au->get_client_pid());
-
                 Device_4_var dev = Device_4::_duplicate(device_4);
-                return (dev->command_inout_4(command.c_str(), any, local_source, ci));
+                return (dev->command_inout_4(command.c_str(), any, local_source, get_client_identification()));
             }
             else if(version >= 2)
             {
@@ -1745,7 +1807,10 @@ void DeviceProxy::real_constructor(const std::string &name, bool need_check_acc)
     {
         if(exported == true)
         {
-            connect(corba_name);
+            // we now use reconnect instead of connect
+            // it allows us to know more about the device we are talking to
+            // see Connection::reconnect for details
+            reconnect(dbase_used);
 
             if(is_alias == true)
             {
@@ -4528,22 +4593,15 @@ void DeviceProxy::set_attribute_config(const AttributeInfoListEx &dev_attr_list)
         {
             check_and_reconnect();
 
-            if(version >= 4)
+            if(version >= 5)
             {
-                ClntIdent ci;
-                ApiUtil *au = ApiUtil::instance();
-                ci.cpp_clnt(au->get_client_pid());
-
-                if(version >= 5)
-                {
-                    Device_5_var dev = Device_5::_duplicate(device_5);
-                    dev->set_attribute_config_5(attr_config_list_5, ci);
-                }
-                else
-                {
-                    Device_4_var dev = Device_4::_duplicate(device_4);
-                    dev->set_attribute_config_4(attr_config_list_3, ci);
-                }
+                Device_5_var dev = Device_5::_duplicate(device_5);
+                dev->set_attribute_config_5(attr_config_list_5, get_client_identification());
+            }
+            else if(version == 4)
+            {
+                Device_4_var dev = Device_4::_duplicate(device_4);
+                dev->set_attribute_config_4(attr_config_list_3, get_client_identification());
             }
             else if(version == 3)
             {
@@ -4801,11 +4859,8 @@ void DeviceProxy::set_pipe_config(const PipeInfoList &dev_pipe_list)
         {
             check_and_reconnect();
 
-            ClntIdent ci;
-            ApiUtil *au = ApiUtil::instance();
-            ci.cpp_clnt(au->get_client_pid());
             Device_5_var dev = Device_5::_duplicate(device_5);
-            dev->set_pipe_config_5(pipe_config_list, ci);
+            dev->set_pipe_config_5(pipe_config_list, get_client_identification());
 
             ctr = 2;
         }
@@ -4921,11 +4976,8 @@ DevicePipe DeviceProxy::read_pipe(const std::string &pipe_name)
         {
             check_and_reconnect();
 
-            ClntIdent ci;
-            ApiUtil *au = ApiUtil::instance();
-            ci.cpp_clnt(au->get_client_pid());
             Device_5_var dev = Device_5::_duplicate(device_5);
-            pipe_value_5 = dev->read_pipe_5(pipe_name.c_str(), ci);
+            pipe_value_5 = dev->read_pipe_5(pipe_name.c_str(), get_client_identification());
 
             ctr = 2;
         }
@@ -5058,11 +5110,8 @@ void DeviceProxy::write_pipe(DevicePipe &dev_pipe)
         {
             check_and_reconnect();
 
-            ClntIdent ci;
-            ApiUtil *au = ApiUtil::instance();
-            ci.cpp_clnt(au->get_client_pid());
             Device_5_var dev = Device_5::_duplicate(device_5);
-            dev->write_pipe_5(pipe_value_5, ci);
+            dev->write_pipe_5(pipe_value_5, get_client_identification());
 
             ctr = 2;
         }
@@ -5188,11 +5237,8 @@ DevicePipe DeviceProxy::write_read_pipe(DevicePipe &pipe_data)
         {
             check_and_reconnect();
 
-            ClntIdent ci;
-            ApiUtil *au = ApiUtil::instance();
-            ci.cpp_clnt(au->get_client_pid());
             Device_5_var dev = Device_5::_duplicate(device_5);
-            r_pipe_value_5 = dev->write_read_pipe_5(pipe_value_5, ci);
+            r_pipe_value_5 = dev->write_read_pipe_5(pipe_value_5, get_client_identification());
 
             ctr = 2;
         }
@@ -5308,19 +5354,15 @@ std::vector<DeviceAttribute> *DeviceProxy::read_attributes(const std::vector<std
         {
             check_and_reconnect(local_source);
 
-            ClntIdent ci;
-            ApiUtil *au = ApiUtil::instance();
-            ci.cpp_clnt(au->get_client_pid());
-
             if(version >= 5)
             {
                 Device_5_var dev = Device_5::_duplicate(device_5);
-                attr_value_list_5 = dev->read_attributes_5(attr_list, local_source, ci);
+                attr_value_list_5 = dev->read_attributes_5(attr_list, local_source, get_client_identification());
             }
             else if(version == 4)
             {
                 Device_4_var dev = Device_4::_duplicate(device_4);
-                attr_value_list_4 = dev->read_attributes_4(attr_list, local_source, ci);
+                attr_value_list_4 = dev->read_attributes_4(attr_list, local_source, get_client_identification());
             }
             else if(version == 3)
             {
@@ -5511,19 +5553,13 @@ DeviceAttribute DeviceProxy::read_attribute(const std::string &attr_string)
 
             if(version >= 5)
             {
-                ClntIdent ci;
-                ApiUtil *au = ApiUtil::instance();
-                ci.cpp_clnt(au->get_client_pid());
                 Device_5_var dev = Device_5::_duplicate(device_5);
-                attr_value_list_5 = dev->read_attributes_5(attr_list, local_source, ci);
+                attr_value_list_5 = dev->read_attributes_5(attr_list, local_source, get_client_identification());
             }
             else if(version == 4)
             {
-                ClntIdent ci;
-                ApiUtil *au = ApiUtil::instance();
-                ci.cpp_clnt(au->get_client_pid());
                 Device_4_var dev = Device_4::_duplicate(device_4);
-                attr_value_list_4 = dev->read_attributes_4(attr_list, local_source, ci);
+                attr_value_list_4 = dev->read_attributes_4(attr_list, local_source, get_client_identification());
             }
             else if(version == 3)
             {
@@ -5609,19 +5645,15 @@ void DeviceProxy::read_attribute(const char *attr_str, DeviceAttribute &dev_attr
         {
             check_and_reconnect(local_source);
 
-            ClntIdent ci;
-            ApiUtil *au = ApiUtil::instance();
-            ci.cpp_clnt(au->get_client_pid());
-
             if(version >= 5)
             {
                 Device_5_var dev = Device_5::_duplicate(device_5);
-                attr_value_list_5 = dev->read_attributes_5(attr_list, local_source, ci);
+                attr_value_list_5 = dev->read_attributes_5(attr_list, local_source, get_client_identification());
             }
             else if(version == 4)
             {
                 Device_4_var dev = Device_4::_duplicate(device_4);
-                attr_value_list_4 = dev->read_attributes_4(attr_list, local_source, ci);
+                attr_value_list_4 = dev->read_attributes_4(attr_list, local_source, get_client_identification());
             }
             else if(version == 3)
             {
@@ -5713,12 +5745,10 @@ void DeviceProxy::read_attribute(const std::string &attr_str, AttributeValue_4 *
         {
             check_and_reconnect(local_source);
 
-            ClntIdent ci;
-            ApiUtil *au = ApiUtil::instance();
-            ci.cpp_clnt(au->get_client_pid());
-
             Device_4_var dev = Device_4::_duplicate(device_4);
-            AttributeValueList_4 *attr_value_list_4 = dev->read_attributes_4(attr_list, local_source, ci);
+            AttributeValueList_4 *attr_value_list_4 =
+                dev->read_attributes_4(attr_list, local_source, get_client_identification());
+
             av_4 = attr_value_list_4->get_buffer(true);
             delete attr_value_list_4;
 
@@ -5771,12 +5801,10 @@ void DeviceProxy::read_attribute(const std::string &attr_str, AttributeValue_5 *
         {
             check_and_reconnect(local_source);
 
-            ClntIdent ci;
-            ApiUtil *au = ApiUtil::instance();
-            ci.cpp_clnt(au->get_client_pid());
-
             Device_5_var dev = Device_5::_duplicate(device_5);
-            AttributeValueList_5 *attr_value_list_5 = dev->read_attributes_5(attr_list, local_source, ci);
+            AttributeValueList_5 *attr_value_list_5 =
+                dev->read_attributes_5(attr_list, local_source, get_client_identification());
+
             av_5 = attr_value_list_5->get_buffer(true);
             delete attr_value_list_5;
 
@@ -6036,12 +6064,8 @@ void DeviceProxy::write_attributes(const std::vector<DeviceAttribute> &attr_list
 
             if(version >= 4)
             {
-                ClntIdent ci;
-                ApiUtil *au = ApiUtil::instance();
-                ci.cpp_clnt(au->get_client_pid());
-
                 Device_4_var dev = Device_4::_duplicate(device_4);
-                dev->write_attributes_4(attr_value_list_4, ci);
+                dev->write_attributes_4(attr_value_list_4, get_client_identification());
             }
             else if(version == 3)
             {
@@ -6306,12 +6330,8 @@ void DeviceProxy::write_attribute(const DeviceAttribute &dev_attr)
 
             if(version >= 4)
             {
-                ClntIdent ci;
-                ApiUtil *au = ApiUtil::instance();
-                ci.cpp_clnt(au->get_client_pid());
-
                 Device_4_var dev = Device_4::_duplicate(device_4);
-                dev->write_attributes_4(attr_value_list_4, ci);
+                dev->write_attributes_4(attr_value_list_4, get_client_identification());
             }
             else if(version == 3)
             {
@@ -6588,12 +6608,9 @@ void DeviceProxy::write_attribute(const AttributeValueList_4 &attr_val)
             // Now, write the attribute(s)
             //
 
-            ClntIdent ci;
-            ApiUtil *au = ApiUtil::instance();
-            ci.cpp_clnt(au->get_client_pid());
-
             Device_4_var dev = Device_4::_duplicate(device_4);
-            dev->write_attributes_4(attr_val, ci);
+            dev->write_attributes_4(attr_val, get_client_identification());
+
             ctr = 2;
         }
         catch(Tango::MultiDevFailed &e)
@@ -8688,7 +8705,7 @@ bool DeviceProxy::get_locker(LockerInfo &lock_info)
 
         if(v_l[1] != 0)
         {
-            lock_info.ll = Tango::CPP;
+            lock_info.ll = Tango::CPP; // TODO: what about the Tango::CPP_6 case
             lock_info.li.LockerPid = v_l[1];
 
             lock_info.locker_class = "Not defined";
@@ -8993,19 +9010,15 @@ DeviceAttribute DeviceProxy::write_read_attribute(const DeviceAttribute &dev_att
             // Now, call the server
             //
 
-            ClntIdent ci;
-            ApiUtil *au = ApiUtil::instance();
-            ci.cpp_clnt(au->get_client_pid());
-
             if(version >= 5)
             {
                 Device_5_var dev = Device_5::_duplicate(device_5);
-                attr_value_list_5 = dev->write_read_attributes_5(attr_value_list, dvsa, ci);
+                attr_value_list_5 = dev->write_read_attributes_5(attr_value_list, dvsa, get_client_identification());
             }
             else
             {
                 Device_4_var dev = Device_4::_duplicate(device_4);
-                attr_value_list_4 = dev->write_read_attributes_4(attr_value_list, ci);
+                attr_value_list_4 = dev->write_read_attributes_4(attr_value_list, get_client_identification());
             }
 
             ctr = 2;
@@ -9260,12 +9273,8 @@ std::vector<DeviceAttribute> *DeviceProxy::write_read_attributes(const std::vect
             // Now, call the server
             //
 
-            ClntIdent ci;
-            ApiUtil *au = ApiUtil::instance();
-            ci.cpp_clnt(au->get_client_pid());
-
             Device_5_var dev = Device_5::_duplicate(device_5);
-            attr_value_list_5 = dev->write_read_attributes_5(attr_value_list, dvsa, ci);
+            attr_value_list_5 = dev->write_read_attributes_5(attr_value_list, dvsa, get_client_identification());
 
             ctr = 2;
         }

src/client/proxy_asyn.cpp
@@ -142,12 +142,8 @@ long Connection::command_inout_asynch(const std::string &command, const DeviceDa
 
     if(version >= 4)
     {
-        ClntIdent ci;
-        ApiUtil *au = ApiUtil::instance();
-        ci.cpp_clnt(au->get_client_pid());
-
         request->add_in_arg() <<= source;
-        request->add_in_arg() <<= ci;
+        request->add_in_arg() <<= get_client_identification();
     }
     else if(version >= 2)
     {
@@ -811,24 +807,18 @@ long DeviceProxy::read_attributes_asynch(const std::vector<std::string> &attr_na
     CORBA::Request_ptr request;
     if(version >= 5)
     {
-        ClntIdent ci;
-        ApiUtil *au = ApiUtil::instance();
-        ci.cpp_clnt(au->get_client_pid());
         request = Connection::device_5->_request("read_attributes_5");
         request->add_in_arg() <<= names;
         request->add_in_arg() <<= source;
-        request->add_in_arg() <<= ci;
+        request->add_in_arg() <<= get_client_identification();
         request->set_return_type(Tango::_tc_AttributeValueList_5);
     }
     else if(version == 4)
     {
-        ClntIdent ci;
-        ApiUtil *au = ApiUtil::instance();
-        ci.cpp_clnt(au->get_client_pid());
         request = Connection::device_4->_request("read_attributes_4");
         request->add_in_arg() <<= names;
         request->add_in_arg() <<= source;
-        request->add_in_arg() <<= ci;
+        request->add_in_arg() <<= get_client_identification();
         request->set_return_type(Tango::_tc_AttributeValueList_4);
     }
     else if(version == 3)
@@ -2017,13 +2007,9 @@ long DeviceProxy::write_attributes_asynch(const std::vector<DeviceAttribute> &at
     CORBA::Request_ptr request;
     if(version >= 4)
     {
-        ClntIdent ci;
-        ApiUtil *au = ApiUtil::instance();
-        ci.cpp_clnt(au->get_client_pid());
-
         request = device_4->_request("write_attributes_4");
         request->add_in_arg() <<= att_4;
-        request->add_in_arg() <<= ci;
+        request->add_in_arg() <<= get_client_identification();
         request->exceptions()->add(Tango::_tc_MultiDevFailed);
     }
     else if(version == 3)
@@ -2102,13 +2088,9 @@ long DeviceProxy::write_attribute_asynch(const DeviceAttribute &attr)
 
     if(version >= 4)
     {
-        ClntIdent ci;
-        ApiUtil *au = ApiUtil::instance();
-        ci.cpp_clnt(au->get_client_pid());
-
         request = device_4->_request("write_attributes_4");
         request->add_in_arg() <<= att_4;
-        request->add_in_arg() <<= ci;
+        request->add_in_arg() <<= get_client_identification();
         request->exceptions()->add(Tango::_tc_MultiDevFailed);
     }
     else if(version == 3)

src/client/proxy_asyn_cb.cpp
@@ -89,12 +89,8 @@ void Connection::command_inout_asynch(const std::string &command, const DeviceDa
 
     if(version >= 4)
     {
-        ClntIdent ci;
-        ApiUtil *au = ApiUtil::instance();
-        ci.cpp_clnt(au->get_client_pid());
-
         req_seq[0]->add_in_arg() <<= source;
-        req_seq[0]->add_in_arg() <<= ci;
+        req_seq[0]->add_in_arg() <<= get_client_identification();
     }
     else if(version >= 2)
     {
@@ -1121,26 +1117,18 @@ void DeviceProxy::read_attributes_asynch(const std::vector<std::string> &attr_na
 
     if(version >= 5)
     {
-        ClntIdent ci;
-        ApiUtil *au = ApiUtil::instance();
-        ci.cpp_clnt(au->get_client_pid());
-
         req_seq[0] = Connection::device_5->_request("read_attributes_5");
         req_seq[0]->add_in_arg() <<= names;
         req_seq[0]->add_in_arg() <<= source;
-        req_seq[0]->add_in_arg() <<= ci;
+        req_seq[0]->add_in_arg() <<= get_client_identification();
         req_seq[0]->set_return_type(Tango::_tc_AttributeValueList_5);
     }
     else if(version == 4)
     {
-        ClntIdent ci;
-        ApiUtil *au = ApiUtil::instance();
-        ci.cpp_clnt(au->get_client_pid());
-
         req_seq[0] = Connection::device_4->_request("read_attributes_4");
         req_seq[0]->add_in_arg() <<= names;
         req_seq[0]->add_in_arg() <<= source;
-        req_seq[0]->add_in_arg() <<= ci;
+        req_seq[0]->add_in_arg() <<= get_client_identification();
         req_seq[0]->set_return_type(Tango::_tc_AttributeValueList_4);
     }
     else if(version == 3)
@@ -1249,13 +1237,9 @@ void DeviceProxy::write_attributes_asynch(const std::vector<DeviceAttribute> &at
 
     if(version >= 4)
     {
-        ClntIdent ci;
-        ApiUtil *au = ApiUtil::instance();
-        ci.cpp_clnt(au->get_client_pid());
-
         req_seq[0] = device_4->_request("write_attributes_4");
         req_seq[0]->add_in_arg() <<= att_4;
-        req_seq[0]->add_in_arg() <<= ci;
+        req_seq[0]->add_in_arg() <<= get_client_identification();
         req_seq[0]->exceptions()->add(Tango::_tc_MultiDevFailed);
     }
     else if(version == 3)
@@ -1347,13 +1331,9 @@ void DeviceProxy::write_attribute_asynch(const DeviceAttribute &attr, CallBack &
 
     if(version >= 4)
     {
-        ClntIdent ci;
-        ApiUtil *au = ApiUtil::instance();
-        ci.cpp_clnt(au->get_client_pid());
-
         req_seq[0] = device_4->_request("write_attributes_4");
         req_seq[0]->add_in_arg() <<= att_4;
-        req_seq[0]->add_in_arg() <<= ci;
+        req_seq[0]->add_in_arg() <<= get_client_identification();
         req_seq[0]->exceptions()->add(Tango::_tc_MultiDevFailed);
     }
     else if(version == 3)

commit 472a4682bfb9060f24c8c6ac689fd790ee911544

server/blackbox.cpp: Avoid copy initialization
This satisfies the performance-unnecessary-copy-initialization
clang-tidy check.  The check was auto-fixed in 6b84cb19 (clang-tidy:
Auto-fix performance-unnecessary-copy-initialization, 2023-09-02) and
has been failing since c3a65891 (add support for new IDL6 client
identification data struct in the client API - #1193, 2023-11-17).

The regression was missed because the clang-tidies warnings-as-errors
was broken in 9a7eb485 (.clang-tidy: Suppress failing warnings,
2023-09-18).

src/server/blackbox.cpp
@@ -429,7 +429,7 @@ void BlackBox::add_cl_ident(const ClntIdent &cl_ident, client_addr *cl_addr)
     break;
     case Tango::JAVA_6:
     {
-        Tango::JavaClntIdent_6 eci = cl_ident.java_clnt_6();
+        const Tango::JavaClntIdent_6 &eci = cl_ident.java_clnt_6();
         Tango::JavaClntIdent jci = eci.java_clnt;
         cl_addr->java_main_class = jci.MainClass;
         cl_addr->java_ident[0] = jci.uuid[0];