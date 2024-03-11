92837a016ef544217e419a849908dc3282169519
Thomas Ives <tri@observatorysciences.co.uk>
2024-03-11 10:29:33 +0000

    ci/windows-build.ps1: Add and use Invoke-NativeCommand
    Now that we are running the build job from a powershell script, the CI
    runner is not running commands directly which means it is not checking
    error codes.
    
    Unfortunately, powershell does not have a `set -e` type option to check
    error codes for commands, so in order to get the script to fail when a
    command returns a non-zero error code we need to check $LASTEXITCODE and
    throw an exception.

--- ci/windows-build.ps1
+++ ci/windows-build.ps1
@@ -1,31 +1,52 @@
+# From https://stackoverflow.com/questions/47032005
+function Invoke-NativeCommand() {
+    if ($args.Count -eq 0) {
+        throw "No arguments."
+    }
+
+    $command = $args[0]
+    $commandArgs = @()
+
+    if ($args.Count -gt 1) {
+        $commandArgs = $args[1..($args.Count - 1)]
+    }
+
+    & $command $commandArgs
+    $result = $LASTEXITCODE
+
+    if ($result -ne 0) {
+        throw "`"$command $commandArgs`" exited with exit code $result."
+    }
+}
+
 $CMAKE_BUILD_PARALLEL_LEVEL=$env:NUMBER_OF_PROCESSORS
 # avoid cmake warning about unknown escape sequences
 $cwd = $(pwd | Convert-Path).Replace("\\", "/")
 # zmq/cppzmq
 $FILENAME="zmq-${ZMQ_VERSION}_${VC_ARCH_VER}.zip"
-curl.exe -JOL https://github.com/tango-controls/zmq-windows-ci/releases/download/${ZMQ_VERSION}/${FILENAME}
+Invoke-NativeCommand curl.exe -JOL https://github.com/tango-controls/zmq-windows-ci/releases/download/${ZMQ_VERSION}/${FILENAME}
 md -Force ${TANGO_ZMQ_ROOT}
 Expand-Archive -Path ${FILENAME} -DestinationPath ${TANGO_ZMQ_ROOT}
 # omniORB
 $FILENAME="omniorb-${OMNI_VERSION}_${VC_ARCH_VER}_${PYVER}.zip"
-curl.exe -JOL https://github.com/tango-controls/omniorb-windows-ci/releases/download/${OMNI_VERSION}/${FILENAME}
+Invoke-NativeCommand curl.exe -JOL https://github.com/tango-controls/omniorb-windows-ci/releases/download/${OMNI_VERSION}/${FILENAME}
 md -Force ${TANGO_OMNI_ROOT}
 Expand-Archive -Path ${FILENAME} -DestinationPath ${TANGO_OMNI_ROOT}
 # pthread
 $FILENAME="pthreads-win32-${PTHREAD_VERSION}_${VC_ARCH_VER}.zip"
-curl.exe -JOL https://github.com/tango-controls/Pthread_WIN32/releases/download/${PTHREAD_VERSION}/${FILENAME}
+Invoke-NativeCommand curl.exe -JOL https://github.com/tango-controls/Pthread_WIN32/releases/download/${PTHREAD_VERSION}/${FILENAME}
 md -Force ${PTHREAD_ROOT}
 Expand-Archive -Path ${FILENAME} -DestinationPath ${PTHREAD_ROOT}
 # nasm (for libjpeg-turbo)
-curl.exe -L ${NASM_DOWNLOAD_LINK} -o nasm.exe
-./nasm.exe /S /v/qn
+Invoke-NativeCommand curl.exe -L ${NASM_DOWNLOAD_LINK} -o nasm.exe
+Invoke-NativeCommand ./nasm.exe /S /v/qn
 # libjpeg-turbo
 $FILENAME="libjpeg-turbo-${JPEG_VERSION}.zip"
-curl.exe -L https://github.com/libjpeg-turbo/libjpeg-turbo/archive/refs/tags/${JPEG_VERSION}.zip -o ${FILENAME}
+Invoke-NativeCommand curl.exe -L https://github.com/libjpeg-turbo/libjpeg-turbo/archive/refs/tags/${JPEG_VERSION}.zip -o ${FILENAME}
 md -Force ${TANGO_JPEG_SOURCE}/..
 Expand-Archive -Path ${FILENAME} -DestinationPath ${TANGO_JPEG_SOURCE}/..
 # Using multiline strings with `>` does not work with powershell and suprisingly plain multiline statements work
-cmake `
+Invoke-NativeCommand cmake `
   -S "${TANGO_JPEG_SOURCE}" `
   -B "${TANGO_JPEG_SOURCE}/build" `
   -G "${CMAKE_GENERATOR}" `
@@ -33,13 +54,13 @@ cmake `
   -DWITH_TURBOJPEG=ON `
   -DCMAKE_INSTALL_PREFIX="${TANGO_JPEG_ROOT}" `
   -DCMAKE_DEBUG_POSTFIX=d
-cmake `
+Invoke-NativeCommand cmake `
   --build "${TANGO_JPEG_SOURCE}/build" `
   --target install `
   --config "${CMAKE_BUILD_TYPE}"
 # opentelemetry with dependencies
 $FILENAME="opentelemetry-with-deps-static-${ARCHITECTURE}.zip"
-curl.exe -JOL "https://gitlab.com/api/v4/projects/54003303/packages/generic/opentelemetry/${OTEL_VERSION}/${FILENAME}"
+Invoke-NativeCommand curl.exe -JOL "https://gitlab.com/api/v4/projects/54003303/packages/generic/opentelemetry/${OTEL_VERSION}/${FILENAME}"
 md -Force ${OTEL_ROOT}
 Expand-Archive -Path ${FILENAME} -DestinationPath ${OTEL_ROOT}
 mv ${OTEL_ROOT}/opentelemetry-cpp*/* ${OTEL_ROOT}
@@ -49,23 +70,23 @@ $LIBCURL_ROOT="${OTEL_ROOT}/libcurl"
 $OPENSSL_ROOT="${LIBCURL_ROOT}"
 # tango idl
 md -Force "${TANGO_IDL_SOURCE}"
-git clone -b ${TANGO_IDL_TAG} --depth 1 --quiet https://gitlab.com/tango-controls/tango-idl ${TANGO_IDL_SOURCE}
-cmake `
+Invoke-NativeCommand git clone -b ${TANGO_IDL_TAG} --depth 1 --quiet https://gitlab.com/tango-controls/tango-idl ${TANGO_IDL_SOURCE}
+Invoke-NativeCommand cmake `
   -S "${TANGO_IDL_SOURCE}" `
   -B "${TANGO_IDL_SOURCE}/build" `
   -G "${CMAKE_GENERATOR}" `
   -A "${ARCHITECTURE}" `
   -DCMAKE_INSTALL_PREFIX="${TANGO_IDL_ROOT}"
-cmake `
+Invoke-NativeCommand cmake `
   --build "${TANGO_IDL_SOURCE}/build" `
   --target install `
   --config "${CMAKE_BUILD_TYPE}"
 # catch
 $FILENAME="catch-${CATCH_VERSION}.zip"
-curl.exe -L https://github.com/catchorg/Catch2/archive/refs/tags/v${CATCH_VERSION}.zip -o ${FILENAME}
+Invoke-NativeCommand curl.exe -L https://github.com/catchorg/Catch2/archive/refs/tags/v${CATCH_VERSION}.zip -o ${FILENAME}
 md -Force ${TANGO_CATCH_SOURCE}/..
 Expand-Archive -Path ${FILENAME} -DestinationPath ${TANGO_CATCH_SOURCE}/..
-cmake `
+Invoke-NativeCommand cmake `
   -S "${TANGO_CATCH_SOURCE}" `
   -B "${TANGO_CATCH_SOURCE}/build" `
   -G "${CMAKE_GENERATOR}" `
@@ -74,22 +95,22 @@ cmake `
   -DCATCH_INSTALL_DOCS=OFF `
   -DCATCH_BUILD_TESTING=OFF `
   -DCATCH_ENABLE_WERROR=OFF
-cmake `
+Invoke-NativeCommand cmake `
   --build "${TANGO_CATCH_SOURCE}/build" `
   --target install `
   --config "${CMAKE_BUILD_TYPE}"
 # wix toolset
 $FILENAME="wix311-binaries.zip"
-curl.exe -JOL https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/${FILENAME}
+Invoke-NativeCommand curl.exe -JOL https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/${FILENAME}
 md -Force ${WIX_TOOLSET_LOCATION}
 Expand-Archive -Path ${FILENAME} -DestinationPath ${WIX_TOOLSET_LOCATION}
 # python (required for building the tests)
 $FILENAME="python-${PYTHON_VERSION}-embed-${PY_ARCH}.zip"
-curl.exe -JOL https://www.python.org/ftp/python/${PYTHON_VERSION}/${FILENAME}
+Invoke-NativeCommand curl.exe -JOL https://www.python.org/ftp/python/${PYTHON_VERSION}/${FILENAME}
 md -FORCE ${PYTHON_LOCATION}
 Expand-Archive -Path ${FILENAME} -DestinationPath ${PYTHON_LOCATION}
 $env:Path += ";${cwd}/${PYTHON_LOCATION}"
-cmake `
+Invoke-NativeCommand cmake `
   -S . `
   -B  build `
   -G "${CMAKE_GENERATOR}" `
@@ -122,10 +143,10 @@ cmake `
   -DOPENSSL_MSVC_STATIC_RT="ON" `
   -DOPENSSL_ROOT_DIR="${cwd}/${OPENSSL_ROOT}" `
   -DTANGO_OTEL_ROOT="${cwd}/${OTEL_ROOT}"
-cmake `
+Invoke-NativeCommand cmake `
   --build build `
   --config "${CMAKE_BUILD_TYPE}"
 # -B not working here, so we have to use cd
 cd build
 # space after `-D` is required
-cpack -D CPACK_WIX_ROOT="${cwd}/${WIX_TOOLSET_LOCATION}" -C "${CMAKE_BUILD_TYPE}" -G "WIX;ZIP"
+Invoke-NativeCommand cpack -D CPACK_WIX_ROOT="${cwd}/${WIX_TOOLSET_LOCATION}" -C "${CMAKE_BUILD_TYPE}" -G "WIX;ZIP"
