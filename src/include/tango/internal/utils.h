e87982730596690351301a3a2faeb06cd110c2c5
Thomas Braun <thomas.braun@byte-physics.de>
2024-04-09 13:04:23 +0200

    detail::stringify_vector: Add it
    This is a generic routine to output vectors with custom separators.

--- src/include/tango/internal/utils.h
+++ src/include/tango/internal/utils.h
@@ -1,8 +1,11 @@
 #ifndef _INTERNAL_UTILS_H
 #define _INTERNAL_UTILS_H
 
+#include <iostream>
+#include <string_view>
 #include <type_traits>
 #include <variant>
+#include <vector>
 
 namespace Tango::detail
 {
@@ -27,6 +30,26 @@ template <class T, class... Ts>
 struct is_one_of<T, std::variant<Ts...>> : std::bool_constant<(std::is_same_v<T, Ts> || ...)>
 {
 };
+
+template <typename T>
+void stringify_vector(std::ostream &os, const std::vector<T> &vec, const std::string_view sep)
+{
+    const auto length = vec.size();
+    if(length == 0)
+    {
+        return;
+    }
+
+    typename std::vector<T>::size_type i;
+
+    TANGO_ASSERT(length > 0);
+    for(i = 0; i < (length - 1); i++)
+    {
+        os << vec[i] << sep;
+    }
+
+    os << vec[i];
+}
 
 } // namespace Tango::detail
 
---
+++ tests/catch2_internal_utils.cpp
@@ -0,0 +1,40 @@
+#include <tango/tango.h>
+#include <utils/utils.h>
+
+#include <tango/internal/utils.h>
+
+template <typename T>
+struct TestData
+{
+    const std::vector<T> vec;
+    const std::string sep;
+    const std::string result;
+};
+
+SCENARIO("stringify_vector behaves as designed")
+{
+    {
+        auto data = GENERATE((TestData<std::string>{{"a", "b"}, ", ", "a, b"}),
+                             (TestData<std::string>{{}, ", ", ""}),
+                             (TestData<std::string>{{"a", "b"}, "", "ab"}),
+                             (TestData<std::string>{{"a"}, ", ", "a"}));
+
+        GIVEN("when serializing a vector with separator " << data.sep)
+        {
+            WHEN("we then expect it to be: " << data.result)
+            {
+                std::stringstream sstr;
+                Tango::detail::stringify_vector(sstr, data.vec, data.sep);
+                REQUIRE(sstr.str() == data.result);
+            }
+        }
+    }
+
+    {
+        auto data = GENERATE((TestData<int>{{1, 2, 3}, "|", "1|2|3"}));
+
+        std::stringstream sstr;
+        Tango::detail::stringify_vector(sstr, data.vec, data.sep);
+        REQUIRE(sstr.str() == data.result);
+    }
+}
