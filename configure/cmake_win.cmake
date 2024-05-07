84a379622d3ec0eb7d0ccbe36c144285b78c2d98
Thomas Braun <thomas.braun@byte-physics.de>
2024-05-07 19:28:18 +0200

    configure/cmake_win.cmake: Bail out if TANGO_OTEL_ROOT is missing
    This variable is as hack to allow us to install all required dependencies
    for telemetry.
    
    But still we want to give early feedback if that is missing.

--- configure/cmake_win.cmake
+++ configure/cmake_win.cmake
@@ -165,6 +165,11 @@ if (TANGO_INSTALL_DEPENDENCIES)
       if(BUILD_SHARED_LIBS)
         message(FATAL_ERROR "Missing installation code")
       else()
+
+        if(NOT TANGO_OTEL_ROOT)
+          message(FATAL_ERROR "Missing TANGO_OTEL_ROOT variable")
+        endif()
+
         install(DIRECTORY ${TANGO_OTEL_ROOT}/lib/ DESTINATION lib)
         install(DIRECTORY ${TANGO_OTEL_ROOT}/cmake DESTINATION lib)
         install(DIRECTORY ${TANGO_OTEL_ROOT}/share/cmake DESTINATION lib)
