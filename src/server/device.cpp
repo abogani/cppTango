abogani/cppTango

commit 355fd2161405f81fb221e5220639fc2d3854a7d0

Resolve "IDL6-version_info implementation"

src/server/device.cpp
@@ -40,7 +40,7 @@
 #include <tango/server/eventsupplier.h>
 #include <tango/client/apiexcept.h>
 #include <tango/server/tango_clock.h>
-
+#include <tango/common/git_revision.h>
 #include <tango/server/logging.h>
 
 namespace Tango
@@ -608,6 +608,17 @@ void DeviceImpl::real_ctor()
 
     init_logger();
 
+    //
+    // Set c++ version_info list values, got from tango_const.h and zmq.h
+    //
+
+    add_version_info("cppTango", TgLibVers);
+    add_version_info("cppTango.git_revision", Tango::git_revision());
+    add_version_info("omniORB", omniORB::versionString());
+    std::ostringstream zmq_version;
+    zmq_version << ZMQ_VERSION_MAJOR << '.' << ZMQ_VERSION_MINOR << '.' << ZMQ_VERSION_PATCH << std::ends;
+    add_version_info("zmq", zmq_version.str());
+
     TANGO_LOG_DEBUG << "Leaving DeviceImpl::real_ctor for device " << device_name << std::endl;
 }
 
@@ -1096,6 +1107,54 @@ void DeviceImpl::get_dev_system_resource()
 PortableServer::POA_ptr DeviceImpl::_default_POA()
 {
     return Util::instance()->get_poa();
+}
+
+//+--------------------------------------------------------------------------------------------------------------------
+//
+// method :
+//		DeviceImpl::add_version_info
+//
+// description :
+//		add key=value pair to version_info list
+//
+//--------------------------------------------------------------------------------------------------------------------
+
+void DeviceImpl::add_version_info(const std::string &key, const std::string &value)
+{
+    version_info.insert_or_assign(key, value);
+    TANGO_LOG_DEBUG << "In DeviceImpl::add_version_info(key = " << key << ", value = " << value << ")" << std::endl;
+}
+
+//+--------------------------------------------------------------------------------------------------------------------
+//
+// method :
+//		DeviceImpl::get_version_info
+//
+// description :
+//		get current libraries versions list
+//
+//--------------------------------------------------------------------------------------------------------------------
+
+Tango::DevInfoVersionList DeviceImpl::get_version_info()
+{
+    //
+    // Convert version_info_list map into a DevInfoVersionList sequence
+    //
+
+    Tango::DevInfoVersionList dev_version_info;
+    int version_length = 0;
+    dev_version_info.length(version_info.size());
+
+    for(const auto &[key, value] : version_info)
+    {
+        Tango::DevInfoVersion version_info_elt;
+        version_info_elt.key = Tango::string_dup(key.c_str());
+        version_info_elt.value = Tango::string_dup(value.c_str());
+        dev_version_info[version_length] = version_info_elt;
+        version_length++;
+    }
+
+    return dev_version_info;
 }
 
 //+-------------------------------------------------------------------------

.gitlab-ci.yml
@@ -20,17 +20,17 @@ variables: &variables
   BUILD_TESTING: "ON"
   TANGO_USE_TELEMETRY: "ON"
   # docker image names and versions
-  DEBIAN_12_IMAGE: "debian12:v7"
-  DEBIAN_12_CROSS_IMAGE: "debian12-cross-32bit:v6"
-  DEBIAN_11_IMAGE: "debian11:v6"
-  UBUNTU_20_IMAGE: "ubuntu-20.04:v7"
-  UBUNTU_20_JPEG9_IMAGE: "ubuntu-20.04-jpeg9:v6"
-  LLVM_LATEST_IMAGE: "llvm-latest:v10"
-  GCC_LATEST_IMAGE: "gcc-latest:v6"
-  FEDORA_IMAGE: "fedora37:v6"
-  ALPINE_IMAGE: "alpine-3.15:v6"
-  DEBIAN_MINIMUM_VERSIONS_IMAGE: "debian-minimum-versions:v6"
-  DEBIAN_MAXIMUM_CMAKE_IMAGE: "debian-maximum-cmake:v6"
+  DEBIAN_12_IMAGE: "debian12:v8"
+  DEBIAN_12_CROSS_IMAGE: "debian12-cross-32bit:v7"
+  DEBIAN_11_IMAGE: "debian11:v7"
+  UBUNTU_20_IMAGE: "ubuntu-20.04:v8"
+  UBUNTU_20_JPEG9_IMAGE: "ubuntu-20.04-jpeg9:v7"
+  LLVM_LATEST_IMAGE: "llvm-latest:v12"
+  GCC_LATEST_IMAGE: "gcc-latest:v7"
+  FEDORA_IMAGE: "fedora37:v7"
+  ALPINE_IMAGE: "alpine-3.15:v7"
+  DEBIAN_MINIMUM_VERSIONS_IMAGE: "debian-minimum-versions:v7"
+  DEBIAN_MAXIMUM_CMAKE_IMAGE: "debian-maximum-cmake:v7"
   GIT_SUBMODULE_STRATEGY: "recursive"
 
 # See: https://docs.gitlab.com/ce/ci/yaml/README.html#workflowrules-templates

.windows-gitlab-ci.yml
@@ -19,7 +19,7 @@ windows-build:
   variables:
     JPEG_VERSION: 2.0.3
     NASM_VERSION: 2.15.05
-    TANGO_IDL_TAG: 6.0-alpha2
+    TANGO_IDL_TAG: 6.0-alpha3
     ZMQ_VERSION: 4.0.5-2
     OMNI_VERSION: 4.3.0
     PTHREAD_VERSION: 2.9.1

src/client/devapi_base.cpp
@@ -3117,6 +3117,7 @@ DeviceInfo const &DeviceProxy::info()
 {
     DevInfo_var dev_info;
     DevInfo_3_var dev_info_3;
+    DevInfo_6_var dev_info_6;
     int ctr = 0;
 
     while(ctr < 2)
@@ -3125,7 +3126,25 @@ DeviceInfo const &DeviceProxy::info()
         {
             check_and_reconnect();
 
-            if(version >= 3)
+            if(version >= 6)
+            {
+                Device_6_var dev = Device_6::_duplicate(device_6);
+                dev_info_6 = dev->info_6();
+
+                _info.dev_class = dev_info_6->dev_class;
+                _info.server_id = dev_info_6->server_id;
+                _info.server_host = dev_info_6->server_host;
+                _info.server_version = dev_info_6->server_version;
+                _info.doc_url = dev_info_6->doc_url;
+                _info.dev_type = dev_info_6->dev_type;
+
+                for(CORBA::ULong i = 0; i < dev_info_6->version_info.length(); ++i)
+                {
+                    Tango::DevInfoVersion version_info = dev_info_6->version_info[i];
+                    _info.version_info.insert(std::make_pair(version_info.key, version_info.value));
+                }
+            }
+            else if(version >= 3)
             {
                 Device_3_var dev = Device_3::_duplicate(device_3);
                 dev_info_3 = dev->info_3();

src/include/tango/client/devapi.h
@@ -149,6 +149,7 @@ struct _DeviceInfo
     long server_version;
     std::string doc_url;
     std::string dev_type;
+    std::map<std::string, std::string> version_info;
 };
 
 typedef _DeviceInfo DeviceInfo;

src/include/tango/server/blackbox.h
@@ -175,7 +175,8 @@ enum BlackBoxElt_OpType
     Op_Set_Pipe_Config_5,
     Op_Read_Pipe_5,
     Op_Write_Pipe_5,
-    Op_Write_Read_Pipe_5
+    Op_Write_Read_Pipe_5,
+    Op_Info_6
 };
 
 class BlackBoxElt

src/include/tango/server/device.h
@@ -377,6 +377,23 @@ class DeviceImpl : public virtual POA_Tango::Device
      */
     PortableServer::POA_ptr _default_POA() override;
 
+    /**
+     * Add version info
+     *
+     * Append to device info current library versions used.
+     *
+     */
+    void add_version_info(const std::string &key, const std::string &value);
+
+    /**
+     * get version info list
+     *
+     * Return a DevInfoVersion list with current libraries
+     * and bindings versions used.
+     *
+     */
+    Tango::DevInfoVersionList get_version_info();
+
     //@}
 
   protected:
@@ -1579,6 +1596,10 @@ class DeviceImpl : public virtual POA_Tango::Device
      * The administration device name
      */
     std::string adm_device_name;
+    /**
+     * The version_info list to store library versions
+     */
+    std::map<std::string, std::string> version_info;
     //@}
 
   public:

src/include/tango/server/device_6.h
@@ -160,7 +160,16 @@ class Device_6Impl : public virtual POA_Tango::Device_6, public Device_5Impl
     /**@name CORBA operation methods
      * Method defined to implement TANGO device CORBA operation */
     //@{
-
+    /**
+     * Get device info.
+     *
+     * Invoked when the client request the info CORBA operation.
+     * It updates the black box and returns a DevInfo_6 object
+     * with miscellaneous device info
+     *
+     * @return A DevInfo object
+     */
+    Tango::DevInfo_6 *info_6() override;
     //@}
 
     /// @privatesection

commit edff854803b43e1d0d55e5aee8e14beaf03a519f

DeviceImpl::real_ctor: Avoid null being part of the returned string
We currently have in the tests logs lines like

attr_polling 19:14:16.465972 DEBUG device.cpp:1125 In DeviceImpl::add_version_info(key = omniORB, value = 4.3.2)
attr_polling 19:14:16.465980 DEBUG device.cpp:1125 In DeviceImpl::add_version_info(key = zmq, value = 4.3.4^@)

and that is because the zmq version string ends in a \0. We don't need to
add that as std::ostringstream::str() returns a std::string.

Bug introduced in 355fd216 (Resolve "IDL6-version_info implementation", 2024-05-02).

src/server/device.cpp
@@ -616,7 +616,7 @@ void DeviceImpl::real_ctor()
     add_version_info("cppTango.git_revision", Tango::git_revision());
     add_version_info("omniORB", omniORB::versionString());
     std::ostringstream zmq_version;
-    zmq_version << ZMQ_VERSION_MAJOR << '.' << ZMQ_VERSION_MINOR << '.' << ZMQ_VERSION_PATCH << std::ends;
+    zmq_version << ZMQ_VERSION_MAJOR << '.' << ZMQ_VERSION_MINOR << '.' << ZMQ_VERSION_PATCH;
     add_version_info("zmq", zmq_version.str());
 
     TANGO_LOG_DEBUG << "Leaving DeviceImpl::real_ctor for device " << device_name << std::endl;