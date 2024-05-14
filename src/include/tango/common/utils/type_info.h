77c5cfe141a6157c6bc498b52b8a3092c505f532
Thomas Ives <tri@observatorysciences.co.uk>
2024-05-14 14:07:05 +0100

    w_attribute.cpp: Extract type name functions to common/utils.cpp
    It would be nice to put these functions in internal/, however, they are
    used from w_attributes_templ.h so they must be installed.

---
+++ src/include/tango/common/utils/type_info.h
@@ -0,0 +1,23 @@
+#ifndef TANGO_COMMON_UTILS_TYPE_INFO_H
+#define TANGO_COMMON_UTILS_TYPE_INFO_H
+
+namespace Tango::detail
+{
+
+/**
+ * @brief Return type name for a CORBA::Any
+ *
+ * @param any The CORBA::Any
+ */
+std::string corba_any_to_type_name(const CORBA::Any &any);
+
+/**
+ * @brief Return the type name for a AttrUnion dtype
+ *
+ * @param d The dtype
+ */
+std::string attr_union_dtype_to_type_name(Tango::AttributeDataType d);
+
+} // namespace Tango::detail
+
+#endif
