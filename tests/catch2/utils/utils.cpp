abogani/cppTango

commit 999664e03d89f3f356da2558f72712ca973c9e8b

tests/bdd/test_attr_manip.cpp: Add catch2 example test
The new bdd::attr_manip test is a port of the very simple existing test old_tests::attr_manip.

tests/bdd/utils/utils.cpp
@@ -1,0 +1,13 @@
+#include "utils/utils.h"
+
+#include <catch2/catch_translate_exception.hpp>
+#include <tango/tango.h>
+
+#include <sstream>
+
+CATCH_TRANSLATE_EXCEPTION(const Tango::DevFailed &ex)
+{
+    std::stringstream ss;
+    Tango::Except::print_exception(ex, ss);
+    return ss.str();
+}

tests/CMakeLists.txt
@@ -233,7 +233,6 @@ set(TESTS asyn_attr_cb
           allowed_cmd
           att_conf
           attr_conf_test
-          attr_manip
           attr_misc
           attr_proxy
           attr_types
@@ -292,6 +291,11 @@ if(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
         COMPILE_FLAGS -Wno-stringop-overflow)
 endif()
 
+add_subdirectory(bdd)
+tango_add_test(NAME "bdd::attr_manip" COMMAND $<TARGET_FILE:BddTests>
+    --warn NoAssertions --filenames-as-tags -s
+    "[#test_attr_manip]")
+
 configure_file(locked_device_cmd.h.cmake locked_device_cmd.h @ONLY)
 target_include_directories(lock PRIVATE ${CMAKE_CURRENT_BINARY_DIR})
 
@@ -329,7 +333,6 @@ tango_add_test(NAME "old_tests::ConfEventBugClient"  COMMAND $<TARGET_FILE:ConfE
 tango_add_test(NAME "old_tests::att_conf"  COMMAND $<TARGET_FILE:att_conf> ${DEV1})
 tango_add_test(NAME "old_tests::attr_conf_test"  COMMAND $<TARGET_FILE:attr_conf_test> ${DEV1})
 tango_add_test(NAME "old_tests::multi_thread_sighandler"  COMMAND $<TARGET_FILE:multi_thread_sighandler>)
-tango_add_test(NAME "old_tests::attr_manip"  COMMAND $<TARGET_FILE:attr_manip> ${DEV1})
 tango_add_test(NAME "old_tests::attr_misc"  COMMAND $<TARGET_FILE:attr_misc> ${DEV1})
 tango_add_test(NAME "old_tests::attr_proxy"  COMMAND $<TARGET_FILE:attr_proxy> ${DEV1}/Short_attr_rw)
 tango_add_test(NAME "old_tests::attr_types"  COMMAND $<TARGET_FILE:attr_types> ${DEV1} 10)

tests/attr_manip.cpp
@@ -1,97 +1,0 @@
-#include "common.h"
-
-int main(int argc, char **argv)
-{
-    DeviceProxy *device;
-
-    if((argc == 1) || (argc > 3))
-    {
-        TEST_LOG << "usage: %s device" << endl;
-        exit(-1);
-    }
-
-    string device_name = argv[1];
-
-    try
-    {
-        device = new DeviceProxy(device_name);
-    }
-    catch(CORBA::Exception &e)
-    {
-        Except::print_exception(e);
-        exit(1);
-    }
-
-    TEST_LOG << endl << "new DeviceProxy(" << device->name() << ") returned" << endl << endl;
-
-    try
-    {
-        //
-        // Get attribute config
-        //
-
-        string att("attr_dq_db");
-        AttributeInfo sta_ai = device->get_attribute_config(att);
-        TEST_LOG << sta_ai << endl;
-
-        //
-        // Get attr value
-        //
-
-        DeviceAttribute da = device->read_attribute(att);
-        TEST_LOG << da << endl;
-        double att_value;
-        da >> att_value;
-
-        //
-        // Change format
-        //
-
-        AttributeInfoList new_ai;
-        new_ai.push_back(sta_ai);
-        new_ai[0].format = "scientific;uppercase;setprecision(2)";
-
-        device->set_attribute_config(new_ai);
-
-        //
-        // Print formatted output
-        //
-
-        AttributeInfo sta_ai_2 = device->get_attribute_config(att);
-        TEST_LOG << sta_ai_2 << endl;
-
-        TEST_LOG << "Formatted value = " << Tango::AttrManip(sta_ai_2.format) << att_value << endl;
-
-        //
-        // Set another format
-        //
-
-        new_ai[0].format = "fixed;setprecision(2)";
-
-        device->set_attribute_config(new_ai);
-
-        //
-        // Print formatted output
-        //
-
-        sta_ai_2 = device->get_attribute_config(att);
-        TEST_LOG << sta_ai_2 << endl;
-
-        TEST_LOG << "Formatted value = " << Tango::AttrManip(sta_ai_2.format) << att_value << endl;
-
-        //
-        // Reset attribute config
-        //
-
-        new_ai[0].format = "";
-
-        device->set_attribute_config(new_ai);
-    }
-    catch(Tango::DevFailed &e)
-    {
-        Except::print_exception(e);
-        exit(-1);
-    }
-
-    return 0;
-}

tests/bdd/CMakeLists.txt
@@ -1,0 +1,8 @@
+find_package(Catch2 3 REQUIRED)
+
+add_executable(BddTests
+    test_attr_manip.cpp
+    utils/utils.cpp)
+
+target_include_directories(BddTests PRIVATE ${CMAKE_CURRENT_SOURCE_DIR})
+target_link_libraries(BddTests PRIVATE tango Catch2::Catch2WithMain)

tests/bdd/test_attr_manip.cpp
@@ -1,0 +1,80 @@
+#include <tango/tango.h>
+
+#include <memory>
+
+#include "utils/utils.h"
+
+// Hard code device name as we plan to not need this
+static constexpr const char *DEVICE_NAME = "test/debian8/10";
+static constexpr double SERVER_VALUE = 8.888;
+
+SCENARIO("attribute formatting can be controlled")
+{
+    GIVEN("a device proxy")
+    {
+        auto device = std::make_unique<Tango::DeviceProxy>(DEVICE_NAME);
+
+        AND_GIVEN("an attribute name and configuration")
+        {
+            std::string att{"attr_dq_db"};
+
+            Tango::AttributeInfo sta_ai;
+            REQUIRE_NOTHROW(sta_ai = device->get_attribute_config(att));
+
+            WHEN("we read the attribute")
+            {
+                Tango::DeviceAttribute da;
+                REQUIRE_NOTHROW(da = device->read_attribute(att));
+
+                THEN("the read value matches the value on the server")
+                {
+                    double att_value;
+                    da >> att_value;
+                    REQUIRE(att_value == SERVER_VALUE);
+                }
+            }
+
+            struct TestData
+            {
+                const char *name;
+                const char *format;
+                const char *expected;
+            };
+
+            auto data = GENERATE((TestData{"scientfic", "scientific;uppercase;setprecision(2)", "8.89E+00"}),
+                                 (TestData{"fixed-width", "fixed;setprecision(2)", "8.89"}));
+
+            AND_GIVEN("a " << data.name << " format specification")
+            {
+                std::string format{data.format};
+
+                WHEN("we set the attribute configuration with the format")
+                {
+                    Tango::AttributeInfoList new_ai;
+                    new_ai.push_back(sta_ai);
+                    new_ai[0].format = format;
+
+                    REQUIRE_NOTHROW(device->set_attribute_config(new_ai));
+
+                    THEN("the format read back matches")
+                    {
+                        Tango::AttributeInfo sta_ai_2;
+                        REQUIRE_NOTHROW(sta_ai_2 = device->get_attribute_config(att));
+                        REQUIRE(sta_ai_2.format == format);
+                    }
+                }
+
+                WHEN("we format a value")
+                {
+                    std::stringstream out;
+                    REQUIRE_NOTHROW(out << Tango::AttrManip(format) << SERVER_VALUE);
+
+                    THEN("the rendered string is in " << data.name << " notation")
+                    {
+                        REQUIRE(out.str() == data.expected);
+                    }
+                }
+            }
+        }
+    }
+}

tests/bdd/utils/utils.h
@@ -1,0 +1,7 @@
+#ifndef TANGO_TESTS_BDD_UTILS_UTILS_H
+#define TANGO_TESTS_BDD_UTILS_UTILS_H
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+
+#endif

commit d0c6ffb0893d0e27d7731d746ef83ffe91a59e83

tests/catch2/utils: Improve exception reporting
`Except::print_exception` actually accepts any `CORBA::Exception` so we
may as well translate these as well.  This has been incorrect since it
was introduced in 999664e0 (tests/bdd/test_attr_manip.cpp: Add catch2
example test, 2023-12-14).

We also might see `omni_thread_fatal` exceptions, so we should translate
those too.

tests/catch2/utils/utils.cpp
@@ -14,10 +14,17 @@
 #include <cstdlib>
 #include <cstdio>
 
-CATCH_TRANSLATE_EXCEPTION(const Tango::DevFailed &ex)
+CATCH_TRANSLATE_EXCEPTION(const CORBA::Exception &ex)
 {
     std::stringstream ss;
     Tango::Except::print_exception(ex, ss);
+    return ss.str();
+}
+
+CATCH_TRANSLATE_EXCEPTION(const omni_thread_fatal &ex)
+{
+    std::stringstream ss;
+    ss << "omni_thread_fatal error: " << strerror(ex.error) << " (" << ex.error << ")";
     return ss.str();
 }
 