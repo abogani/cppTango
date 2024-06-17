abogani/cppTango

commit 9f4c74da99381bcccdb9d5d32b28bd663dd9a35d

Complete opentelemetry addition from previous commit
CMake:
- Link privately against the opentelemetry libraries
- Search for more dependencies to fix compilation on Linux and Windows
- Make TANGO_TELEMETRY_USE_HTTP/TANGO_TELEMETRY_USE_GRPC dependent options
  which are OFF by default but ON when TANGO_USE_TELEMETRY is ON.
- Only define the options in one place
- Move opentelemetry library link calls to set_cflags_and_include

ci/config.sh:
- Add support for TANGO_USE_TELEMETRY

general:
- Explain new cmake options in INSTALL.md

tests:
- Move opentelemetry canary code from cxx_attr_misc.cpp to cxx_telemetry
- Remove more elaborate code which does not compile on windows

configure/cmake_win.cmake
@@ -222,4 +222,15 @@ if (TANGO_INSTALL_DEPENDENCIES)
             install(FILES ${JPEG_RUNTIME_RELEASE} DESTINATION bin COMPONENT dynamic)
         endif()
     endif()
+
+    if(TANGO_USE_TELEMETRY)
+      if(BUILD_SHARED_LIBS)
+        message(FATAL_ERROR "Missing installation code")
+      else()
+        install(DIRECTORY ${TANGO_OTEL_ROOT}/lib/ DESTINATION lib)
+        install(DIRECTORY ${TANGO_OTEL_ROOT}/cmake DESTINATION lib)
+        install(DIRECTORY ${TANGO_OTEL_ROOT}/share/cmake DESTINATION lib)
+        install(DIRECTORY ${TANGO_OTEL_ROOT}/share/pkgconfig DESTINATION lib)
+      endif()
+    endif()
 endif()

CMakeLists.txt
@@ -51,8 +51,9 @@ endif()
 
 # telemetry
 option(TANGO_USE_TELEMETRY "Enable telemetry (OpenTelemetry)" OFF)
-option(TANGO_TELEMETRY_USE_HTTP "Enable http protocol for telemetry (OpenTelemetry) - requires TANGO_USE_TELEMETRY to be ON" OFF)
-option(TANGO_TELEMETRY_USE_GRPC "Enable gRPC protocol for telemetry (OpenTelemetry) - requires TANGO_USE_TELEMETRY to be ON" OFF)
+include(CMakeDependentOption)
+CMAKE_DEPENDENT_OPTION(TANGO_TELEMETRY_USE_HTTP "Enable http protocol for telemetry (OpenTelemetry)" ON "TANGO_USE_TELEMETRY" OFF)
+CMAKE_DEPENDENT_OPTION(TANGO_TELEMETRY_USE_GRPC "Enable gRPC protocol for telemetry (OpenTelemetry)" ON "TANGO_USE_TELEMETRY" OFF)
 
 #need to define the version of the library
 set(MAJOR_VERSION "10")
@@ -104,38 +105,7 @@ add_library(tango $<TARGET_OBJECTS:log4tango_objects>
                   $<TARGET_OBJECTS:idl_objects>
                   $<TARGET_OBJECTS:server_objects>)
 
-target_link_libraries(tango
-    PUBLIC
-        ${CMAKE_DL_LIBS}
-        omniORB4::omniORB4
-        omniORB4::COS4
-        omniORB4::Dynamic4
-        cppzmq::cppzmq
-    )
-
-target_include_directories(tango PUBLIC
-    ${cppzmq_INCLUDE_DIR}
-    ${omniORB4_INCLUDE_DIR}
-)
-
-if(TANGO_USE_JPEG)
-    target_link_libraries(tango PUBLIC JPEG::JPEG)
-endif()
-
-if (TANGO_USE_TELEMETRY)
-    if(NOT TANGO_TELEMETRY_USE_HTTP AND NOT TANGO_TELEMETRY_USE_GRPC)
-        message(FATAL_ERROR 
-        "neither TANGO_TELEMETRY_USE_HTTP nor TANGO_TELEMETRY_USE_GRPC is defined!\n"
-        "use TANGO_TELEMETRY_USE_HTTP and/or TANGO_TELEMETRY_USE_GRPC to select the suported protocol(s).")
-    endif()
-    target_link_libraries(tango PUBLIC opentelemetry-cpp::trace)
-    if (TANGO_TELEMETRY_USE_HTTP)
-        target_link_libraries(tango PUBLIC opentelemetry-cpp::otlp_http_exporter)
-    endif()
-    if (TANGO_TELEMETRY_USE_GRPC)
-        target_link_libraries(tango PUBLIC opentelemetry-cpp::otlp_grpc_exporter)
-    endif()
-endif()
+set_cflags_and_include(tango)
 
 if(WIN32)
     include(configure/cmake_win.cmake)

ci/config.sh
@@ -12,6 +12,7 @@ echo "CMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE"
 echo "CMAKE_DISABLE_PRECOMPILE_HEADERS=$CMAKE_DISABLE_PRECOMPILE_HEADERS"
 echo "BUILD_SHARED_LIBS=$BUILD_SHARED_LIBS"
 echo "TANGO_USE_LIBCPP=$TANGO_USE_LIBCPP"
+echo "TANGO_USE_TELEMETRY=$TANGO_USE_TELEMETRY"
 echo "TANGO_WARNINGS_AS_ERRORS=$TANGO_WARNINGS_AS_ERRORS"
 echo "TOOLCHAIN_FILE=$TOOLCHAIN_FILE"
 echo "CMAKE_EXPORT_COMPILE_COMMANDS=$CMAKE_EXPORT_COMPILE_COMMANDS"
@@ -34,6 +35,7 @@ CMAKE_UNITY_BUILD_BATCH_SIZE=${CMAKE_UNITY_BUILD_BATCH_SIZE:-0}
 TANGO_ENABLE_COVERAGE=${TANGO_ENABLE_COVERAGE:-OFF}
 BUILD_TESTING=${BUILD_TESTING:-ON}
 TANGO_CLANG_FORMAT=${TANGO_CLANG_FORMAT:-OFF}
+TANGO_USE_TELEMETRY=${TANGO_USE_TELEMETRY:-OFF}
 
 ADDITIONAL_ARGS=""
 
@@ -82,4 +84,5 @@ cmake                                                                    \
   -DCMAKE_UNITY_BUILD=${CMAKE_UNITY_BUILD}                               \
   -DCMAKE_UNITY_BUILD_BATCH_SIZE=${CMAKE_UNITY_BUILD_BATCH_SIZE}         \
   -DTANGO_CLANG_FORMAT=${TANGO_CLANG_FORMAT}                             \
+  -DTANGO_USE_TELEMETRY=${TANGO_USE_TELEMETRY}                           \
   ${ADDITIONAL_ARGS}

configure/CMakeLists.txt
@@ -2,9 +2,6 @@ include(GNUInstallDirs)
 
 option(TANGO_USE_JPEG "Build jpeg support" ON)
 option(TANGO_USE_PTHREAD "On windows platform, do we use the pthread library." OFF)
-option(TANGO_USE_TELEMETRY "Enable telemetry (OpenTelemetry)" OFF)
-option(TANGO_TELEMETRY_USE_HTTP "Enable http protocol for telemetry (OpenTelemetry) - requires TANGO_USE_TELEMETRY to be ON" OFF)
-option(TANGO_TELEMETRY_USE_GRPC "Enable gRPC protocol for telemetry (OpenTelemetry) - requires TANGO_USE_TELEMETRY to be ON" OFF)
 
 find_package(Git QUIET)
 if(GIT_FOUND AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.git")
@@ -57,10 +54,19 @@ find_package(tangoidl REQUIRED)
 find_package(omniORB4 REQUIRED)
 
 if (TANGO_USE_TELEMETRY)
-    find_package(protobuf REQUIRED)
+    # workaround gRPC cmake bug fixed in
+    # 5c9e4050 ([Build] fix `gRPCConfig.cmake` (#33361), 2023-09-06)
+    include(CMakeFindDependencyMacro)
+
     find_package(gRPC REQUIRED)
     message(STATUS "Tango telemetry (OpenTelemetry) enabled")
     find_package(opentelemetry-cpp REQUIRED)
+
+    if(WIN32)
+      find_package(CURL REQUIRED)
+      find_package(nlohmann_json REQUIRED)
+      find_package(OpenSSL REQUIRED)
+    endif()
 else()
     message(STATUS "Tango telemetry (OpenTelemetry) disabled")
 endif()

configure/functions.cmake
@@ -19,14 +19,13 @@ function(set_cflags_and_include target)
   endif()
 
   if (TANGO_USE_TELEMETRY)
-    target_link_libraries(${target} PUBLIC opentelemetry-cpp::trace)
-    if (TANGO_TELEMETRY_USE_HTTP)
-      target_link_libraries(${target} PUBLIC opentelemetry-cpp::otlp_http_exporter)
-    endif()
-    if (TANGO_TELEMETRY_USE_GRPC)
-      target_link_libraries(${target} PUBLIC opentelemetry-cpp::otlp_grpc_exporter)
-    endif()
+      target_link_libraries(${target} PRIVATE opentelemetry-cpp::trace opentelemetry-cpp::sdk opentelemetry-cpp::api)
+      if (TANGO_TELEMETRY_USE_HTTP)
+        target_link_libraries(${target} PRIVATE opentelemetry-cpp::otlp_http_exporter)
+      endif()
+      if (TANGO_TELEMETRY_USE_GRPC)
+        target_link_libraries(${target} PRIVATE opentelemetry-cpp::otlp_grpc_exporter)
+      endif()
   endif()
-
 
 endfunction()

src/common/CMakeLists.txt
@@ -1,6 +1,6 @@
 set(git_revision_cpp ${CMAKE_CURRENT_BINARY_DIR}/git_revision.cpp)
 configure_file(git_revision.cpp.in ${git_revision_cpp})
-set(SOURCES net.cpp utils.cpp assert.cpp telemetry.cpp ${git_revision_cpp})
+set(SOURCES net.cpp utils.cpp assert.cpp ${git_revision_cpp})
 
 add_library(common_objects OBJECT ${SOURCES})
 add_dependencies(common_objects idl_objects)

src/include/tango/common/CMakeLists.txt
@@ -1,5 +1,4 @@
 add_subdirectory(utils)
-add_subdirectory(telemetry)
 
 set(tango_version_h ${CMAKE_CURRENT_BINARY_DIR}/tango_version.h)
 configure_file(tango_version.h.in ${tango_version_h})

src/include/tango/common/telemetry/CMakeLists.txt
@@ -1,4 +1,0 @@
-install(
-    FILES
-        telemetry.h
-    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/tango/common/telemetry")

commit 64c04a04c143cc0d71a9b8bbf6ed39b03e14c0c5

configure/cmake_win.cmake: Fix installation folder for opentelemetry dependencies
We can't modify the folder structure as otherwise cmake package
configs are broken and thus find_package for opentelemetry and its
dependencies packages does not work anymore.

Bug introduced in 9f4c74da (Complete opentelemetry addition from previous
commit, 2024-01-25).

configure/cmake_win.cmake
@@ -177,9 +177,9 @@ if (TANGO_INSTALL_DEPENDENCIES)
         endif()
 
         install(DIRECTORY ${TANGO_OTEL_ROOT}/lib/ DESTINATION lib)
-        install(DIRECTORY ${TANGO_OTEL_ROOT}/cmake DESTINATION lib)
-        install(DIRECTORY ${TANGO_OTEL_ROOT}/share/cmake DESTINATION lib)
-        install(DIRECTORY ${TANGO_OTEL_ROOT}/share/pkgconfig DESTINATION lib)
+        install(DIRECTORY ${TANGO_OTEL_ROOT}/cmake/ DESTINATION cmake)
+        install(DIRECTORY ${TANGO_OTEL_ROOT}/share/cmake/ DESTINATION share/cmake)
+        install(DIRECTORY ${TANGO_OTEL_ROOT}/share/pkgconfig/ DESTINATION share/pkgconfig)
       endif()
     endif()
 endif()