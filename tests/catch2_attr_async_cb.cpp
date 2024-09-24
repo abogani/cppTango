0f73155ce4ea22006aa2e78105ac50d96d06b457
Thomas Braun <thomas.braun@byte-physics.de>
2024-09-24 19:13:06 +0200

    tests: Convert asyn_attr_cb to catch2

---
+++ tests/catch2_attr_async_cb.cpp
@@ -0,0 +1,237 @@
+#include "catch2_common.h"
+#include "tango/server/except.h"
+
+namespace
+{
+
+constexpr double ATTR_INIT_VALUE_DP = 9.999;
+constexpr short ATTR_INIT_VALUE_SH = 4711;
+const static std::string TestExceptReason = "Ahhh!";
+
+using CallbackMockType = TangoTest::CallbackMock<Tango::AttrReadEvent, TangoTest::AttrReadEventCopyable>;
+
+std::function<void()> get_poll_func(std::unique_ptr<Tango::DeviceProxy> &device, long timeout)
+{
+    if(timeout < 0)
+    {
+        return [&device]() { device->get_asynch_replies(); };
+    }
+    else
+    {
+        return [&device, timeout]() { device->get_asynch_replies(timeout); };
+    }
+}
+
+} // anonymous namespace
+
+template <class Base>
+class AsyncAttrDev : public Base
+{
+  public:
+    using Base::Base;
+
+    ~AsyncAttrDev() override { }
+
+    void init_device() override { }
+
+    void read_attr(Tango::Attribute &att) override
+    {
+        if(att.get_name() == "attr_asyn")
+        {
+            attr_asyn = ATTR_INIT_VALUE_DP;
+            att.set_value(&attr_asyn);
+        }
+        else if(att.get_name() == "Short_attr")
+        {
+            short_attr = ATTR_INIT_VALUE_SH;
+            att.set_value(&short_attr);
+        }
+        else if(att.get_name() == "attr_asyn_to")
+        {
+            // sleep here intentionally so that the "CORBA read" times out
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            attr_asyn = ATTR_INIT_VALUE_DP;
+            att.set_value(&attr_asyn);
+        }
+        else if(att.get_name() == "attr_asyn_except")
+        {
+            TANGO_THROW_EXCEPTION(TestExceptReason, "This is a test");
+        }
+        else
+        {
+            INFO("Missing case for attribute" << att.get_name());
+            REQUIRE(false);
+        }
+    }
+
+    static void attribute_factory(std::vector<Tango::Attr *> &attrs)
+    {
+        attrs.push_back(new TangoTest::AutoAttr<&AsyncAttrDev::read_attr>("attr_asyn", Tango::DEV_DOUBLE));
+        attrs.push_back(new TangoTest::AutoAttr<&AsyncAttrDev::read_attr>("Short_attr", Tango::DEV_SHORT));
+        attrs.push_back(new TangoTest::AutoAttr<&AsyncAttrDev::read_attr>("attr_asyn_except", Tango::DEV_DOUBLE));
+        attrs.push_back(new TangoTest::AutoAttr<&AsyncAttrDev::read_attr>("attr_asyn_to", Tango::DEV_DOUBLE));
+    }
+
+  private:
+    Tango::DevDouble attr_asyn;
+    Tango::DevShort short_attr;
+};
+
+TANGO_TEST_AUTO_DEV_TMPL_INSTANTIATE(AsyncAttrDev, 1)
+
+SCENARIO("Querying device with get_asynch_replies")
+{
+    int idlver = GENERATE(TangoTest::idlversion(1));
+    GIVEN("a device proxy to a simple IDLv" << idlver << " device")
+    {
+        TangoTest::Context ctx{"attr_asyn", "AsyncAttrDev", idlver};
+        auto device = ctx.get_proxy();
+
+        REQUIRE(idlver == device->get_idl_version());
+
+        CallbackMockType callback;
+        device->read_attribute_asynch("attr_asyn", callback);
+
+        auto timeout = GENERATE(0, 500, -1);
+        AND_GIVEN("we can read with timeout: " << timeout)
+        {
+            auto poll_func = get_poll_func(device, timeout);
+
+            auto event = callback.pop_next_event(poll_func);
+
+            AND_THEN("and get the attribute value")
+            {
+                using namespace Catch::Matchers;
+                using namespace TangoTest::Matchers;
+
+                REQUIRE(event != std::nullopt);
+
+                REQUIRE_THAT(event,
+                             EventValueMatches(AnyMatch(AnyLikeMatches(WithinAbs(ATTR_INIT_VALUE_DP, 0.0000001)))));
+
+                std::vector<std::string> names{"attr_asyn"};
+                REQUIRE_THAT(event->attr_names, RangeEquals(names));
+            }
+        }
+    }
+}
+
+SCENARIO("Device timeout with get_asynch_replies")
+{
+    int idlver = GENERATE(TangoTest::idlversion(1));
+    GIVEN("a device proxy to a simple IDLv" << idlver << " device")
+    {
+        TangoTest::Context ctx{"attr_asyn", "AsyncAttrDev", idlver};
+        auto device = ctx.get_proxy();
+
+        REQUIRE(idlver == device->get_idl_version());
+
+        // tweaking the device timeout allows us to execute the test faster
+        device->set_timeout_millis(500);
+
+        CallbackMockType callback;
+        device->read_attribute_asynch("attr_asyn_to", callback);
+
+        auto timeout = GENERATE(0, 500, -1);
+        AND_GIVEN("we can read with timeout: " << timeout)
+        {
+            auto poll_func = get_poll_func(device, timeout);
+
+            auto event = callback.pop_next_event(poll_func);
+
+            AND_THEN("get an error")
+            {
+                using namespace Catch::Matchers;
+                using namespace TangoTest::Matchers;
+
+                REQUIRE(event != std::nullopt);
+                REQUIRE_THAT(event, EventErrorMatches(AnyMatch(Reason(Tango::API_DeviceTimedOut))));
+
+                std::vector<std::string> names{"attr_asyn_to"};
+                REQUIRE_THAT(event->attr_names, RangeEquals(names));
+            }
+        }
+    }
+}
+
+SCENARIO("Device exception with get_asynch_replies")
+{
+    int idlver = GENERATE(TangoTest::idlversion(1));
+    GIVEN("a device proxy to a simple IDLv" << idlver << " device")
+    {
+        TangoTest::Context ctx{"attr_asyn", "AsyncAttrDev", idlver};
+        auto device = ctx.get_proxy();
+
+        REQUIRE(idlver == device->get_idl_version());
+
+        CallbackMockType callback;
+        device->read_attribute_asynch("attr_asyn_except", callback);
+
+        auto timeout = GENERATE(0, 500, -1);
+        AND_GIVEN("we can read with timeout: " << timeout)
+        {
+            auto poll_func = get_poll_func(device, timeout);
+
+            auto event = callback.pop_next_event(poll_func);
+
+            AND_THEN("get an error")
+            {
+                using namespace Catch::Matchers;
+                using namespace TangoTest::Matchers;
+
+                REQUIRE(event != std::nullopt);
+
+                if(idlver >= 3)
+                {
+                    REQUIRE_THAT(event, EventErrorMatches(AllMatch(Reason(Tango::API_AttributeFailed))));
+                    REQUIRE_THAT(event->argout, SizeIs(1));
+                    REQUIRE_THAT(event->argout[0], ErrorListMatchesMatcher(AllMatch(Reason(TestExceptReason))));
+                }
+                else
+                {
+                    // we only have the event error list and no per attribute error list
+                    // so we need to check with AnyMatch for both of them
+                    REQUIRE_THAT(event, EventErrorMatches(AnyMatch(Reason(Tango::API_AttributeFailed))));
+                    REQUIRE_THAT(event, EventErrorMatches(AnyMatch(Reason(TestExceptReason))));
+                    REQUIRE_THAT(event->argout, IsEmpty());
+                }
+
+                std::vector<std::string> names{"attr_asyn_except"};
+                REQUIRE_THAT(event->attr_names, RangeEquals(names));
+            }
+        }
+    }
+}
+
+SCENARIO("multiple attributes can be read asynchronously")
+{
+    int idlver = GENERATE(TangoTest::idlversion(1));
+    GIVEN("a device proxy to a simple IDLv" << idlver << " device")
+    {
+        TangoTest::Context ctx{"attr_asyn", "AsyncAttrDev", idlver};
+        auto device = ctx.get_proxy();
+
+        REQUIRE(idlver == device->get_idl_version());
+        AND_GIVEN("we read multiple attributes asynchronously")
+        {
+            std::vector<std::string> names{"attr_asyn", "short_attr"};
+
+            CallbackMockType callback;
+            device->read_attributes_asynch(names, callback);
+
+            AND_THEN("get a result back")
+            {
+                using namespace Catch::Matchers;
+                using namespace TangoTest::Matchers;
+
+                auto event = callback.pop_next_event([&]() { device->get_asynch_replies(); });
+                REQUIRE(event != std::nullopt);
+                REQUIRE(event->device != nullptr);
+                REQUIRE_THAT(event->attr_names, RangeEquals(names));
+                REQUIRE_THAT(event,
+                             EventValueMatches(AnyMatch(AnyLikeMatches(WithinAbs(ATTR_INIT_VALUE_DP, 0.0000001)))));
+                REQUIRE_THAT(event, EventValueMatches(AnyMatch(AnyLikeContains(ATTR_INIT_VALUE_SH))));
+            }
+        }
+    }
+}
