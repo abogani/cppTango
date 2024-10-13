65f51d9f51e01c86dad9a37368e8386270f77cdd
Thomas Braun <thomas.braun@byte-physics.de>
2024-10-13 15:04:43 +0200

    tests/catch2_misc.cpp: Better checks for DeviceInfo
    By checking the size we can catch missing adaptations on future changes to
    DeviceInfo. This is also enough if we only do that on Linux.
    
    Also check the version_info keys. With the slight complication that we
    have the opentelemetry key only when telemetry support is enabled.

--- tests/catch2_misc.cpp
+++ tests/catch2_misc.cpp
@@ -86,6 +86,11 @@ SCENARIO("Various device properties")
 
             CAPTURE(info);
 
+#ifdef linux
+            INFO("Tests need to be adapted for changed DeviceInfo structure");
+            REQUIRE(sizeof(info) == 216);
+#endif
+
             REQUIRE(info.dev_class == "MiscDev_" + std::to_string(idlver));
             REQUIRE(info.dev_type == "Uninitialised");
             REQUIRE(info.doc_url == "Doc URL = http://www.tango-controls.org");
@@ -93,6 +98,31 @@ SCENARIO("Various device properties")
             REQUIRE_THAT(info.server_host, !IsEmpty());
             REQUIRE(info.server_id == "TestServer/misc");
             REQUIRE(info.server_version == 6);
+
+            if(idlver < 6)
+            {
+                REQUIRE_THAT(info.version_info, IsEmpty());
+            }
+            else
+            {
+                std::vector<std::string> info_keys;
+                for(const auto &[key, value] : info.version_info)
+                {
+                    REQUIRE_THAT(key, !IsEmpty());
+                    REQUIRE_THAT(value, !IsEmpty());
+
+                    info_keys.emplace_back(key);
+                }
+
+                std::vector<std::string> ref_keys = {
+                    "cppTango", "cppTango.git_revision", "cppzmq", "idl", "omniORB", "zmq"};
+
+#if defined(TANGO_USE_TELEMETRY)
+                ref_keys.emplace_back("opentelemetry-cpp");
+#endif
+
+                REQUIRE_THAT(info_keys, UnorderedRangeEquals(ref_keys));
+            }
         }
     }
 }
