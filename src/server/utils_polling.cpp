35ec088d58d6042e876c9f2fd7fb9c0355ba0ad3
Damien Lacoste <damien.lacoste@esrf.fr>
2025-04-21 00:11:57 +0200

    server/utils_polling.cpp: replace tango/tango.h include

--- src/server/utils_polling.cpp
+++ src/server/utils_polling.cpp
@@ -29,8 +29,9 @@
 //
 //-==================================================================================================================
 
-#include <tango/tango.h>
 #include <tango/server/tango_clock.h>
+#include <tango/server/dserver.h>
+#include <tango/server/command.h>
 #include <tango/client/Database.h>
 
 #include <iostream>
