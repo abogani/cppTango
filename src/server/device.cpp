abogani/cppTango

commit 5f6629ca7ce18d8a515d8a09508190443952c491

Rearrange includes
In tango/client one could find headers imported from inside a namespace
from another header.
Namely in devapi.h: ApiUtil.h, DeviceData.h, DevicePipe.h and
DeviceAttribute.h
and in DbApi.h: Database.h and DbDevice.h
With this work some of the cyclic dependencies are broken, code was
moved from headers to implementation to have a saner set of headers and
get closer to be able to work with including only the necessary headers
and not tango/tango.h (even though this one was updated to keep working)

src/server/device.cpp
@@ -42,6 +42,7 @@
 #include <tango/server/tango_clock.h>
 #include <tango/common/git_revision.h>
 #include <tango/server/logging.h>
+#include <tango/client/DbDevice.h>
 #include <tango/internal/telemetry/telemetry_kernel_macros.h>
 
 #if defined(TANGO_USE_TELEMETRY)
@@ -6428,6 +6429,21 @@ void DeviceImpl::set_pipe_event_subscription_states(const PipeEventSubscriptionS
             pipe.set_event_subscription(now);
         }
     }
+}
+
+inline DbDevice *DeviceImpl::get_db_device()
+{
+    if(!Tango::Util::instance()->use_db())
+    {
+        TangoSys_OMemStream desc_mess;
+        desc_mess << "Method not available for device ";
+        desc_mess << device_name;
+        desc_mess << " which is a non database device";
+
+        TANGO_THROW_EXCEPTION(API_NonDatabaseDevice, desc_mess.str());
+    }
+
+    return db_dev;
 }
 
 } // namespace Tango

commit 6e3b658ba505f5eb17b51c744443cf4c880359a4

device.cpp: Remove bogus inline specifier
We can't inline a public method. Broken since 5f6629ca (Rearrange includes, 2025-04-05).

Close #1428

src/server/device.cpp
@@ -6427,7 +6427,7 @@ void DeviceImpl::set_pipe_event_subscription_states(const PipeEventSubscriptionS
     }
 }
 
-inline DbDevice *DeviceImpl::get_db_device()
+DbDevice *DeviceImpl::get_db_device()
 {
     if(!Tango::Util::instance()->use_db())
     {